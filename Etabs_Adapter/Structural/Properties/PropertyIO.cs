ad7eab711302018a0968f87356a2647f2f9b52e1
EddyDalton <edward.dalton@burohappold.com>
2016-09-05 17:26:03 +0100

    Section and panel IO updated

--- Etabs_Adapter/Structural/Properties/PropertyIO.cs
+++ Etabs_Adapter/Structural/Properties/PropertyIO.cs
@@ -60,9 +60,8 @@ namespace Etabs_Adapter.Structural.Properties
             }
 
             SapModel.PropMaterial.GetMaterial(material, ref matType, ref colour, ref notes, ref guid);
-            SectionType sectionType = GetSectionType(matType, isColumn);
-
-            return new SectionProperty(outType, sectionType, t3, t2, tf, tw, 0, 0, 0, t2, tb2, tfb);
+           
+            return new SectionProperty(outType, t3, t2, tf, tw, 0, 0, 0, t2, tb2, tfb);
         }
 
         public static PanelProperty GetPanelProperty(cSapModel SapModel, string name, out string materialName)
@@ -82,14 +81,17 @@ namespace Etabs_Adapter.Structural.Properties
             {
                 case 0:
                     SapModel.PropArea.GetSlab(name, ref sType, ref shType, ref material, ref t2, ref colour, ref notes, ref guid);
-                    break;
+                    materialName = material;
+                    return new ConstantThickness(name, t2, PanelType.Slab);
                 case 1:
                     eWallPropType wType = eWallPropType.AutoSelectList;
                     SapModel.PropArea.GetWall(name, ref wType, ref shType, ref material, ref t2, ref colour, ref notes, ref guid);
-                    break;
+                    materialName = material;
+                    return new ConstantThickness(name, t2, PanelType.Wall);
+                default:
+                    materialName = "";
+                    return null;
             }
-            materialName = material;
-            return new ConstantThickness(name, t2);
         }
 
         public static void CreatePanelProperty(cSapModel SapModel, bool wall, PanelProperty p, Material material)
@@ -103,20 +105,20 @@ namespace Etabs_Adapter.Structural.Properties
 
             if (!wall)
             {
-                switch (p.Type)
+                if (p is Ribbed)
                 {
-                    case PanelType.Constant:
-                    case PanelType.Drop:
-                        SapModel.PropArea.SetSlab(p.Name, eSlabType.Slab, eShellType.ShellThin, material.Name, p.Thickness);
-                        break;
-                    case PanelType.Ribbed:
-                        Ribbed rT = p as Ribbed;
-                        SapModel.PropArea.SetSlabRibbed(p.Name, rT.TotalDepth, rT.Thickness, rT.StemWidth, rT.StemWidth, rT.Spacing, rT.Direction == PanelDirection.X ? 1 : 0);
-                        break;
-                    case PanelType.Waffle:
-                        Waffle wT = p as Waffle;
-                        SapModel.PropArea.SetSlabWaffle(p.Name, wT.TotalDepthX, wT.Thickness, wT.StemWidthX, wT.StemWidthX, wT.SpacingX, wT.SpacingY);
-                        break;
+                    Ribbed rT = p as Ribbed;
+                    SapModel.PropArea.SetSlabRibbed(p.Name, rT.TotalDepth, rT.Thickness, rT.StemWidth, rT.StemWidth, rT.Spacing, rT.Direction == PanelDirection.X ? 1 : 0);
+                }
+                else if (p is Waffle)
+                {
+                    Waffle wT = p as Waffle;
+                    SapModel.PropArea.SetSlabWaffle(p.Name, wT.TotalDepthX, wT.Thickness, wT.StemWidthX, wT.StemWidthX, wT.SpacingX, wT.SpacingY);
+                }
+                else
+                {
+                    SapModel.PropArea.SetSlab(p.Name, eSlabType.Slab, eShellType.ShellThin, material.Name, p.Thickness);
+
                 }
             }
             else
@@ -133,58 +135,65 @@ namespace Etabs_Adapter.Structural.Properties
 
         public static void CreateBarProperty(cSapModel SapModel, SectionProperty b, Material material)
         {
-            if (material == null)
-            {
-                material = Material.Default(b.SectionMaterial);
-            }
+            string materialName = "";
 
-            if (material != null) EtabsUtils.CreateMaterial(SapModel, material);
+            if (material != null)
+            {
+                EtabsUtils.CreateMaterial(SapModel, material);
+                materialName = material.Name;
+            }
+            else
+            {
+                int num = 0;
+                string[] names = null;
+                SapModel.PropMaterial.GetNameList(ref num, ref names);
+                materialName = names[0];
+            }
 
             switch (b.Shape)
             {
                 case ShapeType.ISection:
-                    SapModel.PropFrame.SetISection(b.Name, material.Name, b.TotalDepth, b.SectionData[(int)SteelSectionData.B1],
+                    SapModel.PropFrame.SetISection(b.Name, materialName, b.TotalDepth, b.SectionData[(int)SteelSectionData.B1],
                         b.SectionData[(int)SteelSectionData.TF1], b.SectionData[(int)SteelSectionData.TW],
                         b.SectionData[(int)SteelSectionData.B2], b.SectionData[(int)SteelSectionData.TF2]);
                     break;
                 case ShapeType.Box:
-                    SapModel.PropFrame.SetTube(b.Name, material.Name, b.TotalDepth, b.TotalWidth, b.SectionData[(int)SteelSectionData.TF1], b.SectionData[(int)SteelSectionData.TW]);
+                    SapModel.PropFrame.SetTube(b.Name, materialName, b.TotalDepth, b.TotalWidth, b.SectionData[(int)SteelSectionData.TF1], b.SectionData[(int)SteelSectionData.TW]);
                     break;
                 case ShapeType.Circle:
-                    SapModel.PropFrame.SetCircle(b.Name, material.Name, b.TotalDepth);
+                    SapModel.PropFrame.SetCircle(b.Name, materialName, b.TotalDepth);
                     break;
                 case ShapeType.Tube:
-                    SapModel.PropFrame.SetPipe(b.Name, material.Name, b.TotalDepth, b.SectionData[(int)SteelSectionData.TF1]);
+                    SapModel.PropFrame.SetPipe(b.Name, materialName, b.TotalDepth, b.SectionData[(int)SteelSectionData.TF1]);
                     break;
                 case ShapeType.Rectangle:
-                    SapModel.PropFrame.SetRectangle(b.Name, material.Name, b.TotalDepth, b.TotalWidth);
+                    SapModel.PropFrame.SetRectangle(b.Name, materialName, b.TotalDepth, b.TotalWidth);
                     break;
                 case ShapeType.Angle:
-                    SapModel.PropFrame.SetAngle(b.Name, material.Name, b.TotalDepth, b.TotalWidth, b.SectionData[(int)SteelSectionData.TF1], b.SectionData[(int)SteelSectionData.TW]);
+                    SapModel.PropFrame.SetAngle(b.Name, materialName, b.TotalDepth, b.TotalWidth, b.SectionData[(int)SteelSectionData.TF1], b.SectionData[(int)SteelSectionData.TW]);
                     break;
                 case ShapeType.Tee:
-                    SapModel.PropFrame.SetTee(b.Name, material.Name, b.TotalDepth, b.TotalWidth, b.SectionData[(int)SteelSectionData.TF1], b.SectionData[(int)SteelSectionData.TW]);
+                    SapModel.PropFrame.SetTee(b.Name, materialName, b.TotalDepth, b.TotalWidth, b.SectionData[(int)SteelSectionData.TF1], b.SectionData[(int)SteelSectionData.TW]);
                     break;
             }
         }
-
-      
-        public static SectionType GetSectionType(eMatType m, bool IsColumn)
-        {
-            switch (m)
-            {
-                case eMatType.Concrete:
-                    return IsColumn ? SectionType.ConcreteColumn : SectionType.ConcreteBeam;
-                case eMatType.Steel:
-                    return SectionType.Steel;
-                case eMatType.NoDesign:
-                    return SectionType.Timber;
-                case eMatType.Aluminum:
-                    return SectionType.Aluminium;
-                default:
-                    return SectionType.Steel;
-            }
-        }
+    
+        //public static SectionType GetSectionType(eMatType m, bool IsColumn)
+        //{
+        //    switch (m)
+        //    {
+        //        case eMatType.Concrete:
+        //            return IsColumn ? SectionType.ConcreteColumn : SectionType.ConcreteBeam;
+        //        case eMatType.Steel:
+        //            return SectionType.Steel;
+        //        case eMatType.NoDesign:
+        //            return SectionType.Timber;
+        //        case eMatType.Aluminum:
+        //            return SectionType.Aluminium;
+        //        default:
+        //            return SectionType.Steel;
+        //    }
+        //}
 
         public static NodeConstraint GetNodeConstraint(bool[] restraint, double[] values)
         {
