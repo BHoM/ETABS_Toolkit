c5618a1b1ae8e463e6a72991464e857f62bbf279
BUROHAPPOLD\mhenriks <martin.henriksen@burohappold.com>
2018-01-19 10:38:15 +0100

    convert class

--- Etabs_Adapter/Convert/ToETABS.cs
+++ Etabs_Adapter/Convert/ToETABS.cs
@@ -3,11 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ETABS2016;
+using BH.oM.Base;
+using BH.oM.Structural.Elements;
 
 namespace BH.Adapter.ETABS
 {
     public static partial class Convert
     {
+        public static cPointObj ToETABSNode(Node bhNode, cSapModel model)
+        {
+
+        }
+
 
     }
 }
