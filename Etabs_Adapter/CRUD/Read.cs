1726b677f276a35523fefdfcf5fc63a98222d536
BUROHAPPOLD\mhenriks <martin.henriksen@burohappold.com>
2018-02-02 17:38:38 +0100

    add readNodes scafolding as example - not for this PR

--- Etabs_Adapter/CRUD/Read.cs
+++ Etabs_Adapter/CRUD/Read.cs
@@ -18,7 +18,21 @@ namespace BH.Adapter.ETABS
             else if (type == typeof(Bar))
                 return new List<Bar>();
 
-            return null;//<--- returning null will throw error in replace method line 34: can't do typeof(null) - returning null makes sense and is used in adapters (GSA)
+            return null;//<--- returning null will throw error in replace method of BHOM_Adapter line 34: can't do typeof(null) - returning null does seem the most sensible to return though
+        }
+
+        private List<Node> ReadNodes(List<int> ids = null)
+        {
+            List<Node> nodeList = new List<Node>();
+            if (ids == null)
+            {
+                //get all nodes
+            }
+            else
+            {
+                //get nodes by id
+            }
+            return nodeList;
         }
 
     }
