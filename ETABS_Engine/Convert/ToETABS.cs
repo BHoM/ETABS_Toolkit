5085ada7cc380845974cd3ca1be46b438b3f6e57
BUROHAPPOLD\mhenriks <martin.henriksen@burohappold.com>
2018-02-02 15:40:55 +0100

    node default id assignment

--- ETABS_Engine/Convert/ToETABS.cs
+++ ETABS_Engine/Convert/ToETABS.cs
@@ -28,30 +28,41 @@ namespace BH.Engine.ETABS
             // as long as tags are unique it should be ok to store tags here - it looks like all elements have get/set for GUID.
             // 'name' and 'lable' (non editable from UI and API) 
 
-            string name = bhNode.CustomData[AdapterId].ToString();
+            string name = "";
+            if (!bhNode.CustomData.ContainsKey(AdapterId))
+                bhNode.CustomData.Add(AdapterId, "thisWillBeChanged");
+            else
+                name = bhNode.CustomData[AdapterId].ToString();
+            
             model.PointObj.AddCartesian(bhNode.Position.X, bhNode.Position.Y, bhNode.Position.Z, ref name);
 
             //TODO: update the BHOM Node with the id acctually assigned in ETABS, or don't - not sure which would best align with behaviour of other adapters
             //bhNode.CustomData[AdapterId] = name;
 
-            bool[] restraint = new bool[6];
-            restraint[0] = bhNode.Constraint.TranslationX == DOFType.Fixed;
-            restraint[1] = bhNode.Constraint.TranslationY == DOFType.Fixed;
-            restraint[2] = bhNode.Constraint.TranslationZ == DOFType.Fixed;
-            restraint[3] = bhNode.Constraint.RotationX == DOFType.Fixed;
-            restraint[4] = bhNode.Constraint.RotationY == DOFType.Fixed;
-            restraint[5] = bhNode.Constraint.RotationZ == DOFType.Fixed;
+            //if (bhNode.CustomData.ContainsKey(AdapterId))
+            //    bhNode.CustomData.Add(AdapterId, name);
 
-            double[] spring = new double[6];
-            spring[0] = bhNode.Constraint.TranslationalStiffnessX;
-            spring[1] = bhNode.Constraint.TranslationalStiffnessY;
-            spring[2] = bhNode.Constraint.TranslationalStiffnessZ;
-            spring[3] = bhNode.Constraint.RotationalStiffnessX;
-            spring[4] = bhNode.Constraint.RotationalStiffnessY;
-            spring[5] = bhNode.Constraint.RotationalStiffnessZ;
+            if(bhNode.Constraint != null)
+            {
+                bool[] restraint = new bool[6];
+                restraint[0] = bhNode.Constraint.TranslationX == DOFType.Fixed;
+                restraint[1] = bhNode.Constraint.TranslationY == DOFType.Fixed;
+                restraint[2] = bhNode.Constraint.TranslationZ == DOFType.Fixed;
+                restraint[3] = bhNode.Constraint.RotationX == DOFType.Fixed;
+                restraint[4] = bhNode.Constraint.RotationY == DOFType.Fixed;
+                restraint[5] = bhNode.Constraint.RotationZ == DOFType.Fixed;
 
-            model.PointObj.SetRestraint(name, ref restraint);
-            model.PointObj.SetSpring(name, ref spring);
+                double[] spring = new double[6];
+                spring[0] = bhNode.Constraint.TranslationalStiffnessX;
+                spring[1] = bhNode.Constraint.TranslationalStiffnessY;
+                spring[2] = bhNode.Constraint.TranslationalStiffnessZ;
+                spring[3] = bhNode.Constraint.RotationalStiffnessX;
+                spring[4] = bhNode.Constraint.RotationalStiffnessY;
+                spring[5] = bhNode.Constraint.RotationalStiffnessZ;
+
+                model.PointObj.SetRestraint(name, ref restraint);
+                model.PointObj.SetSpring(name, ref spring);
+            }
 
             ////it is likely that the tags used needs to be unique to work in etabs - add object id or guid to the tag hashset?
             //model.PointObj.SetGUID(name, bhNode.TaggedName());
@@ -61,6 +72,12 @@ namespace BH.Engine.ETABS
 
         public static void ToETABS(this Bar bhBar, cSapModel model)
         {
+            string name = bhBar.CustomData[AdapterId].ToString();
+            //Note: there are 2 ways of adding Bars 
+            //(A) add using existing points - this will fail if the endnodes have not been pushed and assigned valid AdapterId as CustomData
+            //(B) add using coordinates - could this potentially add duplicate nodes? it is unclear how ETABS handles duplicate/coincident nodes internally
+
+            #region bar using method (A)
             //get all node ids to chack if bar is using a node that has already been pushed... if this is not handled elsewhere in the BHoMAdapter already
             int ptCount = 0;
             string[] ids = null;
@@ -73,13 +90,15 @@ namespace BH.Engine.ETABS
 
             string ptA;
             string ptB;
-            string name = bhBar.CustomData[AdapterId].ToString();
+
+            bhBar.StartNode.ToETABS(model);
+            bhBar.EndNode.ToETABS(model);
 
             bool startExists = ids.Contains(bhBar.StartNode.CustomData[AdapterId].ToString());
             bool endExists = ids.Contains(bhBar.EndNode.CustomData[AdapterId].ToString());
             bool bothExists = endExists == true && startExists == true;
 
-            if(bothExists)
+            if (bothExists)
             {
                 ptA = bhBar.StartNode.CustomData[AdapterId].ToString();
                 ptB = bhBar.EndNode.CustomData[AdapterId].ToString();
@@ -98,10 +117,17 @@ namespace BH.Engine.ETABS
             {
                 ptA = bhBar.StartNode.ToETABS(model);
                 ptB = bhBar.EndNode.ToETABS(model);
-
             }
 
             model.FrameObj.AddByPoint(ptA, ptB, ref name);
+            #endregion
+
+            #region Bar using method (B)
+
+
+            //model.FrameObj.AddByCoord()
+            #endregion
+
             //model.FrameObj.SetGUID(name, bhNode.TaggedName());// see comment on node convert
             //model.FrameObj.SetSection();
             //model.FrameObj.SetReleases();
