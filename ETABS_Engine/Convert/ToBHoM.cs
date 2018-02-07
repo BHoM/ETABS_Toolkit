4288a8ac9e697f1315971d5b5a1b1637308c16ba
BUROHAPPOLD\mhenriks <martin.henriksen@burohappold.com>
2018-02-07 17:27:01 +0100

    change convert.ToBHoM class to mode generic Read method

--- ETABS_Engine/Convert/ToBHoM.cs
+++ ETABS_Engine/Convert/ToBHoM.cs
@@ -13,9 +13,45 @@ namespace BH.Engine.ETABS
 {
     public static partial class Convert
     {
-        public static Node ToBHoM(this cPointObj point)
+        /// <summary>
+        /// ids = null returns all nodes in model
+        /// </summary>
+        public static List<Node> ToBHoM(this cPointObj pointObj, List<string> ids)
         {
-            
+            List<Node> bhNodes = new List<Node>();
+            int nameCount = 0;
+            string[] nameArr = { };
+
+            if (ids == null)
+            {
+                pointObj.GetNameList(ref nameCount, ref nameArr);
+                ids = nameArr.ToList();
+            }
+
+            double x, y, z;
+            x = y = z = 0;
+
+            foreach (string id in ids)
+            {
+                Node bhNode = new Node();
+                pointObj.GetCoordCartesian(id, ref x, ref y, ref z);
+                bhNode.Position = new oM.Geometry.Point() { X = x, Y = y, Z = z };
+                bhNode.CustomData.Add(AdapterId, id);
+
+                //add constraints etsc. ...
+
+
+                bhNodes.Add(bhNode);
+            }
+            return bhNodes;
+        }
+
+
+        public static Node ToBHoM(this cPointObj point, string id)
+        {
+            // use this inside the above List<Node> overload method ?!!! 
+
+
             string name = "noPoint"; //wtf? there looks to be no way fo getting the coordinates withouot the name/id of the node!!!
             double x = 0;
             double y = 0;
@@ -27,7 +63,7 @@ namespace BH.Engine.ETABS
             bhNode.Position = new oM.Geometry.Point { X = x, Y = y, Z = z };
 
             //add conversion for other properties !!!
-
+            
             return bhNode;
         }
          
