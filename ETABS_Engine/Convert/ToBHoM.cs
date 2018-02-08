e4d97eb42857cbece40a896bcb5d80ffde349aff
BUROHAPPOLD\mhenriks <martin.henriksen@burohappold.com>
2018-02-08 12:00:21 +0100

    refactor node .ToBHoM removing redundanccies

--- ETABS_Engine/Convert/ToBHoM.cs
+++ ETABS_Engine/Convert/ToBHoM.cs
@@ -28,64 +28,46 @@ namespace BH.Engine.ETABS
                 ids = nameArr.ToList();
             }
 
+            foreach (string id in ids)
+            {
+                bhNodes.Add(pointObj.ToBHoM(id));
+            }
+
+            return bhNodes;
+        }
+
+
+        public static Node ToBHoM(this cPointObj pointObj, string id)
+        {
+            Node bhNode = new Node();
             double x, y, z;
             x = y = z = 0;
             bool[] restraint = new bool[6];
             double[] spring = new double[6];
 
+            pointObj.GetCoordCartesian(id, ref x, ref y, ref z);
+            bhNode.Position = new oM.Geometry.Point() { X = x, Y = y, Z = z };
+            bhNode.CustomData.Add(AdapterId, id);
 
-            foreach (string id in ids)
-            {
-                Node bhNode = new Node();
-                pointObj.GetCoordCartesian(id, ref x, ref y, ref z);
-                bhNode.Position = new oM.Geometry.Point() { X = x, Y = y, Z = z };
-                bhNode.CustomData.Add(AdapterId, id);
+            restraint[0] = bhNode.Constraint.TranslationX == DOFType.Fixed;
+            restraint[1] = bhNode.Constraint.TranslationY == DOFType.Fixed;
+            restraint[2] = bhNode.Constraint.TranslationZ == DOFType.Fixed;
+            restraint[3] = bhNode.Constraint.RotationX == DOFType.Fixed;
+            restraint[4] = bhNode.Constraint.RotationY == DOFType.Fixed;
+            restraint[5] = bhNode.Constraint.RotationZ == DOFType.Fixed;
+            pointObj.GetRestraint(id, ref restraint);
 
-                restraint[0] = bhNode.Constraint.TranslationX == DOFType.Fixed;
-                restraint[1] = bhNode.Constraint.TranslationY == DOFType.Fixed;
-                restraint[2] = bhNode.Constraint.TranslationZ == DOFType.Fixed;
-                restraint[3] = bhNode.Constraint.RotationX == DOFType.Fixed;
-                restraint[4] = bhNode.Constraint.RotationY == DOFType.Fixed;
-                restraint[5] = bhNode.Constraint.RotationZ == DOFType.Fixed;
-                pointObj.GetRestraint(id, ref restraint);
+            spring[0] = bhNode.Constraint.TranslationalStiffnessX;
+            spring[1] = bhNode.Constraint.TranslationalStiffnessY;
+            spring[2] = bhNode.Constraint.TranslationalStiffnessZ;
+            spring[3] = bhNode.Constraint.RotationalStiffnessX;
+            spring[4] = bhNode.Constraint.RotationalStiffnessY;
+            spring[5] = bhNode.Constraint.RotationalStiffnessZ;
+            pointObj.SetSpring(id, ref spring);
 
-
-                spring[0] = bhNode.Constraint.TranslationalStiffnessX;
-                spring[1] = bhNode.Constraint.TranslationalStiffnessY;
-                spring[2] = bhNode.Constraint.TranslationalStiffnessZ;
-                spring[3] = bhNode.Constraint.RotationalStiffnessX;
-                spring[4] = bhNode.Constraint.RotationalStiffnessY;
-                spring[5] = bhNode.Constraint.RotationalStiffnessZ;
-                pointObj.SetSpring(id, ref spring);
-
-
-
-                bhNodes.Add(bhNode);
-            }
-            return bhNodes;
-        }
-
-
-        public static Node ToBHoM(this cPointObj point, string id)
-        {
-            // use this inside the above List<Node> overload method ?!!! 
-
-
-            string name = "noPoint"; //wtf? there looks to be no way fo getting the coordinates withouot the name/id of the node!!!
-            double x = 0;
-            double y = 0;
-            double z = 0;
-            point.GetCoordCartesian(name, ref x, ref y, ref z);
-
-            Node bhNode = new Node();
-            bhNode.Name = name;
-            bhNode.Position = new oM.Geometry.Point { X = x, Y = y, Z = z };
-
-            //add conversion for other properties !!!
-            
             return bhNode;
         }
-         
+
 
         //public static Bar ToBHoM(this cFrameObj bar)
         //{
