0ea5a517c79ff013bcacb25248cdb5d15d439965
BUROHAPPOLD\mhenriks <martin.henriksen@burohappold.com>
2018-02-09 18:12:51 +0100

    Convert .ToBHoM for common section types - same as previous adapter version

--- ETABS_Engine/Convert/PrivateHelpers/Section.cs
+++ ETABS_Engine/Convert/PrivateHelpers/Section.cs
@@ -12,27 +12,62 @@ namespace BH.Engine.ETABS
     {
         private static ISectionProperty GetSectionProperty(cSapModel model, string propertyName, eFramePropType propertyType)
         {
-            ISectionProperty sectionProperty = null;
+            ISectionProperty bhSectionProperty = null;
+            ISectionDimensions dimensions = null;
             string materialName = "";
+
+            string fileName = "";
+            double t3 = 0;
+            double t2 = 0;
+            double tf = 0;
+            double tw = 0;
+            double tfb = 0;
+            double t2b = 0;
+
+            int colour = 0;
+            string notes = "";
+            string guid = "";
+
 
             switch (propertyType)
             {
                 case eFramePropType.I:
+                    model.PropFrame.GetISection(propertyName, ref fileName, ref materialName, ref t3, ref t2, ref tf, ref tw, ref t2b, ref tfb, ref colour, ref notes, ref guid);
+                    if (false)//TODO: check if standard or fabricated
+                        dimensions = new StandardISectionDimensions(t3, t2, tw, tf, 0, 0);
+                    else
+                        dimensions = new FabricatedISectionDimensions(t3, t2, t2b, tw, tf, tfb, 0);
+                    break;
                 case eFramePropType.Channel:
+                    model.PropFrame.GetChannel(propertyName, ref fileName, ref materialName, ref t3, ref t2, ref tf, ref tw, ref colour, ref notes, ref guid);
+                    dimensions = new StandardChannelSectionDimensions(t3, t2, tw, tf, 0, 0);
                     break;
                 case eFramePropType.T:
                     break;
                 case eFramePropType.Angle:
                     break;
                 case eFramePropType.DblAngle:
+                    model.PropFrame.GetAngle(propertyName, ref fileName, ref materialName, ref t3, ref t2, ref tf, ref tw, ref colour, ref notes, ref guid);
+                    dimensions = new StandardAngleSectionDimensions(t3, t2, tw, tf, 0, 0);
                     break;
                 case eFramePropType.Box:
+                    model.PropFrame.GetTube(propertyName, ref fileName, ref materialName, ref t3, ref t2, ref tf, ref tw, ref colour, ref notes, ref guid);
+                    if (tf == tw)
+                        dimensions = new StandardBoxDimensions(t3, t2, tf, 0, 0);
+                    else
+                        dimensions = new FabricatedBoxDimensions(t3, t2, tw, tf, tf, 0);
                     break;
                 case eFramePropType.Pipe:
+                    model.PropFrame.GetPipe(propertyName, ref fileName, ref materialName, ref t3, ref tw, ref colour, ref notes, ref guid);
+                    dimensions = new TubeDimensions(t3, tw);
                     break;
                 case eFramePropType.Rectangular:
+                    model.PropFrame.GetRectangle(propertyName, ref fileName, ref materialName, ref t3, ref t2, ref colour, ref notes, ref guid);
+                    dimensions = new RectangleSectionDimensions(t3, t2, 0);
                     break;
                 case eFramePropType.Circle:
+                    model.PropFrame.GetCircle(propertyName, ref fileName, ref materialName, ref t3, ref colour, ref notes, ref guid);
+                    dimensions = new CircleDimensions(t3);
                     break;
                 case eFramePropType.General:
                     break;
@@ -97,12 +132,15 @@ namespace BH.Engine.ETABS
                 case eFramePropType.SteelRod:
                     break;
                 default:
-                    break;
+                    throw new NotImplementedException("Section convertion for the type: " + propertyType.ToString() + "is not implmented in ETABS adapter");
             }
+            if(dimensions==null)
+                throw new NotImplementedException("Section convertion for the type: " + propertyType.ToString() + "is not implmented in ETABS adapter");
 
-            sectionProperty.Material = GetMaterial(model, materialName);
 
-            return sectionProperty;
+            bhSectionProperty.Material = GetMaterial(model, materialName);
+
+            return bhSectionProperty;
         }
 
     }
