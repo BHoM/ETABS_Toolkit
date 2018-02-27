f512b0b654d23438e09a6befbc8390c3651de6bf
BUROHAPPOLD\mhenriks <martin.henriksen@burohappold.com>
2018-02-27 11:33:36 +0100

    move .ToETABS functionality to Create

--- Etabs_Adapter/CRUD/Create.cs
+++ Etabs_Adapter/CRUD/Create.cs
@@ -4,6 +4,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using BH.Engine.ETABS;
+using BH.oM;
+using BH.oM.Structural;
+using BH.oM.Structural.Elements;
+using BH.oM.Structural.Properties;
 
 namespace BH.Adapter.ETABS
 {
@@ -17,7 +21,10 @@ namespace BH.Adapter.ETABS
             {
                 foreach (T obj in objects)
                 {
-                    ((BH.oM.Base.IBHoMObject)obj).ToETABS(modelData);
+                    success = CreateObject(obj as dynamic, modelData);
+                    //if (!success)
+                    //    break;
+                    //((BH.oM.Base.IBHoMObject)obj).ToETABS(modelData);
                 }
             }
             else
@@ -29,5 +36,93 @@ namespace BH.Adapter.ETABS
             return success;
         }
 
+        private bool CreateObject(Node bhNode, ModelData modelData)
+        {
+            bool success = true;
+            int retA = 0;
+            int retB = 0;
+            int retC = 0;
+
+            string name = "";
+            string bhId = bhNode.CustomData[AdapterId].ToString();
+            name = bhId;
+
+            retA = modelData.model.PointObj.AddCartesian(bhNode.Position.X, bhNode.Position.Y, bhNode.Position.Z, ref name);
+            if (name != bhId)
+                success = false; //this is not necessary if you can guarantee that it is impossible that this bhId does not match any existing name in ETABS !!!
+
+            if (bhNode.Constraint != null)
+            {
+                bool[] restraint = new bool[6];
+                restraint[0] = bhNode.Constraint.TranslationX == DOFType.Fixed;
+                restraint[1] = bhNode.Constraint.TranslationY == DOFType.Fixed;
+                restraint[2] = bhNode.Constraint.TranslationZ == DOFType.Fixed;
+                restraint[3] = bhNode.Constraint.RotationX == DOFType.Fixed;
+                restraint[4] = bhNode.Constraint.RotationY == DOFType.Fixed;
+                restraint[5] = bhNode.Constraint.RotationZ == DOFType.Fixed;
+
+                double[] spring = new double[6];
+                spring[0] = bhNode.Constraint.TranslationalStiffnessX;
+                spring[1] = bhNode.Constraint.TranslationalStiffnessY;
+                spring[2] = bhNode.Constraint.TranslationalStiffnessZ;
+                spring[3] = bhNode.Constraint.RotationalStiffnessX;
+                spring[4] = bhNode.Constraint.RotationalStiffnessY;
+                spring[5] = bhNode.Constraint.RotationalStiffnessZ;
+
+                retB = modelData.model.PointObj.SetRestraint(name, ref restraint);
+                retC = modelData.model.PointObj.SetSpring(name, ref spring);
+            }
+
+            if (retA != 0 || retB != 0 || retC != 0)
+                success = false;
+
+            return success;
+        }
+
+        private bool CreateObject(Bar bhBar, ModelData modelData)
+        {
+            bool success = true;
+            int retA = 0;
+            int retB = 0;
+            int retC = 0;
+
+            string name = "";
+            string bhId = bhBar.CustomData[AdapterId].ToString();
+            name = bhId;
+
+            retA = modelData.model.FrameObj.AddByPoint(bhBar.StartNode.CustomData[AdapterId].ToString(), bhBar.EndNode.CustomData[AdapterId].ToString(), ref name);
+            if (bhId != name)
+                success = false;
+
+            //model.FrameObj.SetGUID(name, bhNode.TaggedName());// see comment on node convert
+            retB = modelData.model.FrameObj.SetSection(name, bhBar.SectionProperty.Name);
+            //model.FrameObj.SetReleases();
+            //model.FrameObj.SetGroupAssign();
+            if (retA != 0 || retB != 0 || retC != 0)
+                success = false;
+
+            return success;
+        }
+
+        private bool CreateObject(ISectionProperty bhSection, ModelData modelData)
+        {
+            bool success = true;
+
+            BH.Engine.ETABS.Convert.SetSectionProperty(modelData, bhSection);//TODO: this is only halfway done - should be moved away from engine to adapter as much as possible
+
+            return success;
+        }
+
+        //private bool CreateObject(PanelPlanar bhPanel, ModelData modelData)
+        //{
+        //    bool success = true;
+
+        //    bhPanel.ExternalEdges = null;
+        //    int segmentCount
+
+        //    modelData.model.AreaObj.AddByCoord()
+
+        //    return success;
+        //}
     }
 }
