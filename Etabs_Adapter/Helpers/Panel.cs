5486b53f2ccacd59e6da46bd426731765a3ece18
BUROHAPPOLD\mhenriks <martin.henriksen@burohappold.com>
2018-03-14 14:25:34 +0100

    added GetPerimeter-helper method

---
+++ Etabs_Adapter/Helpers/Panel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ETABS2016;
+using BH.oM.Geometry;
+
+namespace BH.Adapter.ETABS
+{
+    public partial class Helper
+    {
+        public static Polyline GetPanelPerimeter(cSapModel model, string id)
+        {
+            string[] pName = null;
+            int pointCount = 0;
+            double pX1 = 0;
+            double pY1 = 0;
+            double pZ1 = 0;
+            model.AreaObj.GetPoints(id, ref pointCount, ref pName);
+            List<Point> pts = new List<Point>();
+            for (int j = 0; j < pointCount; j++)
+            {
+                model.PointObj.GetCoordCartesian(pName[j], ref pX1, ref pY1, ref pZ1);
+                pts.Add(new Point() { X = pX1, Y = pY1, Z = pZ1 });
+            }
+            pts.Add(pts[0]);
+
+            Polyline pl = new Polyline() { ControlPoints = pts };
+
+            return pl;
+        }
+    }
+}
