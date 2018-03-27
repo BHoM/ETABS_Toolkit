d0430e713c20b2173da13ba19f3781f496dd36ed
MartinHenriksen <martin.henriksen@burohappold.com>
2018-03-27 17:17:11 +0200

    setup structure for all results - not tested

---
+++ Etabs_Adapter/CRUD/ReadResults.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BH.oM.Common;
+using BH.oM.Structural.Results;
+
+namespace BH.Adapter.ETABS
+{
+    public partial class ETABSAdapter
+    {
+
+        protected override IEnumerable<IResult> ReadResults(Type type, IList ids = null, IList cases = null, int divisions = 5)
+        {
+            if (typeof(StructuralGlobalResult).IsAssignableFrom(type))
+                return GetGlobalResults(type, cases);
+            else
+                return GetObjectResults(type, ids, cases, divisions);
+
+        }
+
+        private IEnumerable<IResult> GetGlobalResults(Type type, IList cases)
+        {
+            if (typeof(GlobalReactions).IsAssignableFrom(type))
+                return GetGlobalReactions(cases);
+            if (typeof(ModalDynamics).IsAssignableFrom(type))
+                throw new NotImplementedException("modal dynamics not supported yet");
+
+            return new List<IResult>();
+
+        }
+
+        private IEnumerable<IResult> GetGlobalReactions(IList cases)
+        {
+
+
+            return new List<IResult>();
+
+        }
+
+        private IEnumerable<IResult> GetObjectResults(Type type, IList ids = null, IList cases = null, int divisions = 5)
+        {
+            if (type == typeof(NodeResult))
+                return GetNodeResults(type, ids, cases, divisions);
+            else if (type == typeof(BarResult))
+                return GetBarResults(type, ids, cases, divisions);
+            else if (type == typeof(PanelResult))
+                return GetPanelResults(type, ids, cases, divisions);
+            else
+                return new List<IResult>();
+        }
+
+        private List<IResult> GetNodeResults(Type type, IList ids = null, IList cases = null, int divisions = 5)
+        {
+            if (type == typeof(NodeAcceleration))
+                return Helper.GetNodeAcceleration(model, ids, cases, divisions);
+            else if (type == typeof(NodeDisplacement))
+                return Helper.GetNodeDisplacement(model, ids, cases, divisions);
+            else if (type == typeof(NodeReaction))
+                return Helper.GetNodeReaction(model, ids, cases, divisions);
+            else if (type == typeof(NodeVelocity))
+                return Helper.GetNodeVelocity(model, ids, cases, divisions);
+
+
+            return new List<IResult>();
+        }
+
+        private List<IResult> GetBarResults(Type type, IList ids = null, IList cases = null, int divisions = 5)
+        {
+            if (type == typeof(BarDeformation))
+                return Helper.GetBarDeformation(model, ids, cases, divisions);
+            else if (type == typeof(BarForce))
+                return Helper.GetBarForce(model, ids, cases, divisions);
+            else if (type == typeof(BarStrain))
+                return Helper.GetBarStrain(model, ids, cases, divisions);
+            else if (type == typeof(BarStress))
+                return Helper.GetBarStress(model, ids, cases, divisions);
+
+
+            return new List<IResult>();
+        }
+
+        private List<IResult> GetPanelResults(Type type, IList ids = null, IList cases = null, int divisions = 5)
+        {
+            if (type == typeof(PanelForce))
+                return Helper.GetPanelForce(model, ids, cases, divisions);
+            else if (type == typeof(PanelStress))
+                return Helper.GetPanelStress(model, ids, cases, divisions);
+
+            return new List<IResult>();
+        }
+
+    }
+}
---
+++ Etabs_Adapter/Helpers/Results.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BH.oM.Structural.Results;
+using BH.oM.Common;
+using ETABS2016;
+
+namespace BH.Adapter.ETABS
+{
+    public static partial class Helper
+    {
+        #region Node Results
+
+        public static List<IResult> GetNodeAcceleration(cSapModel model, IList ids = null, IList cases = null, int divisions = 5)
+        {
+
+            return new List<IResult>();
+        }
+
+        public static List<IResult> GetNodeDisplacement(cSapModel model, IList ids = null, IList cases = null, int divisions = 5)
+        {
+
+            return new List<IResult>();
+        }
+
+        public static List<IResult> GetNodeReaction(cSapModel model, IList ids = null, IList cases = null, int divisions = 5)
+        {
+
+            return new List<IResult>();
+        }
+
+        public static List<IResult> GetNodeVelocity(cSapModel model, IList ids = null, IList cases = null, int divisions = 5)
+        {
+
+            return new List<IResult>();
+        }
+        #endregion
+
+        #region bar Results
+
+        public static List<IResult> GetBarDeformation(cSapModel model, IList ids = null, IList cases = null, int divisions = 5)
+        {
+
+            return new List<IResult>();
+        }
+
+        public static List<IResult> GetBarForce(cSapModel model, IList ids = null, IList cases = null, int divisions = 5)
+        {
+
+            return new List<IResult>();
+        }
+
+        public static List<IResult> GetBarStrain(cSapModel model, IList ids = null, IList cases = null, int divisions = 5)
+        {
+
+            return new List<IResult>();
+        }
+
+        public static List<IResult> GetBarStress(cSapModel model, IList ids = null, IList cases = null, int divisions = 5)
+        {
+
+            return new List<IResult>();
+        }
+
+
+        #endregion
+
+        #region Panel Results
+
+        public static List<IResult> GetPanelForce(cSapModel model, IList ids = null, IList cases = null, int divisions = 5)
+        {
+
+            return new List<IResult>();
+        }
+
+        public static List<IResult> GetPanelStress(cSapModel model, IList ids = null, IList cases = null, int divisions = 5)
+        {
+
+            return new List<IResult>();
+        }
+
+
+        #endregion
+    }
+}
