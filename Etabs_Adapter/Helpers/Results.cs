2eeffcaf698c48eca48957cf2cc8861e88b66d3f
MartinHenriksen <martin.henriksen@burohappold.com>
2018-04-04 16:44:55 +0200

    added helper for bar force results

--- Etabs_Adapter/Helpers/Results.cs
+++ Etabs_Adapter/Helpers/Results.cs
@@ -14,7 +14,7 @@ namespace BH.Adapter.ETABS
     {
         #region Node Results
 
-        public static List<IResult> GetNodeAcceleration(cSapModel model, IList ids = null, IList cases = null, int divisions = 5)
+        public static List<NodeResult> GetNodeAcceleration(cSapModel model, IList ids = null, IList cases = null, int divisions = 5)
         {
 
             throw new NotImplementedException("Node Acceleration results is not supported yet!");
@@ -177,7 +177,7 @@ namespace BH.Adapter.ETABS
             return nodeReactions;
         }
 
-        public static List<IResult> GetNodeVelocity(cSapModel model, IList ids = null, IList cases = null, int divisions = 5)
+        public static List<NodeResult> GetNodeVelocity(cSapModel model, IList ids = null, IList cases = null, int divisions = 5)
         {
             throw new NotImplementedException("Node Acceleration results is not supported yet!");
 
@@ -186,28 +186,116 @@ namespace BH.Adapter.ETABS
 
         #region bar Results
 
-        public static List<IResult> GetBarDeformation(cSapModel model, IList ids = null, IList cases = null, int divisions = 5)
+        public static List<BarResult> GetBarDeformation(cSapModel model, IList ids = null, IList cases = null, int divisions = 5)
         {
 
-            return new List<IResult>();
+            throw new NotImplementedException("Bar deformation results is not supported yet!");
         }
 
-        public static List<IResult> GetBarForce(cSapModel model, IList ids = null, IList cases = null, int divisions = 5)
+        public static List<BarForce> GetBarForce(cSapModel model, IList ids = null, IList cases = null, int divisions = 5)
         {
+            List<string> loadcaseIds = new List<string>();
+            List<string> barIds = new List<string>();
+            List<BarForce> barForces = new List<BarForce>();
 
-            return new List<IResult>();
+            if (ids == null)
+            {
+                int bars = 0;
+                string[] names = null;
+                model.FrameObj.GetNameList(ref bars, ref names);
+                barIds = names.ToList();
+            }
+
+            if (cases == null)
+            {
+                int casesCount = 0;
+                string[] names = null;
+                model.LoadCases.GetNameList(ref casesCount, ref names);
+                loadcaseIds = names.ToList();
+                model.RespCombo.GetNameList(ref casesCount, ref names);
+                loadcaseIds.AddRange(names);
+            }
+
+
+            int resultCount = 0;
+            string[] loadcaseNames = null;
+            string[] objects = null;
+            string[] elm = null;
+            double[] objStation = null;
+            double[] elmStation = null;
+            double[] stepNum = null;
+            string[] stepType = null;
+
+            double[] fx = null;
+            double[] fy = null;
+            double[] fz = null;
+            double[] mx = null;
+            double[] my = null;
+            double[] mz = null;
+
+            int type = 0;
+            double segSize = 0;
+            bool op1 = false;
+            bool op2 = false;
+
+
+            model.Results.Setup.DeselectAllCasesAndCombosForOutput();
+
+            for (int loadcase = 0; loadcase < loadcaseIds.Count; loadcase++)
+            {
+                if (model.Results.Setup.SetCaseSelectedForOutput(loadcaseIds[loadcase]) != 0)
+                {
+                    model.Results.Setup.SetComboSelectedForOutput(loadcaseIds[loadcase]);
+                }
+            }
+
+            //List<BarForce<string, string, string>> barForces = new List<BarForce<string, string, string>>();
+            int counter = 1;
+            for (int i = 0; i < barIds.Count; i++)
+            {
+                model.FrameObj.GetOutputStations(barIds[i], ref type, ref segSize, ref divisions, ref op1, ref op2);
+                int ret = model.Results.FrameForce(barIds[i], eItemTypeElm.ObjectElm, ref resultCount, ref objects, ref objStation, ref elm, ref elmStation,
+                ref loadcaseNames, ref stepType, ref stepNum, ref fx, ref fy, ref fz, ref mx, ref my, ref mz);
+                if (ret == 0)
+                {
+                    for (int j = 0; j < resultCount; j++)
+                    {
+                        //string step = stepType[j] != null ? stepType[j] == "Max" ? " Max" : stepType[j] == "Min" ? " Min" : "1" : "0";
+                        //if (objStation[j] == 0)
+                        //    counter = 1;
+                        //barForces.Add(new BarForce<string, string, string>(objects[j], loadcaseNames[j], counter++, divisions, step, fx[j], fz[j], fy[j], mx[j], mz[j], my[j]));
+
+                        BarForce bf = new BarForce();
+                        bf.Case = loadcaseIds[j];
+                        bf.ObjectId = barIds[i];
+                        bf.MX = mx[j];
+                        bf.MY = my[j];
+                        bf.MZ = mz[j];
+                        bf.FX = fx[j];
+                        bf.FY = fy[j];
+                        bf.FZ = fz[j];
+                        bf.Divisions = divisions;
+                        bf.Position = objStation[j];
+                        bf.TimeStep = stepNum[j];
+                        barForces.Add(bf);
+                    }
+                }
+            }
+
+            return barForces;
+
         }
 
-        public static List<IResult> GetBarStrain(cSapModel model, IList ids = null, IList cases = null, int divisions = 5)
+        public static List<BarResult> GetBarStrain(cSapModel model, IList ids = null, IList cases = null, int divisions = 5)
         {
 
-            return new List<IResult>();
+            throw new NotImplementedException("Bar strain results is not supported yet!");
         }
 
-        public static List<IResult> GetBarStress(cSapModel model, IList ids = null, IList cases = null, int divisions = 5)
+        public static List<BarResult> GetBarStress(cSapModel model, IList ids = null, IList cases = null, int divisions = 5)
         {
 
-            return new List<IResult>();
+            throw new NotImplementedException("Bar stress results is not supported yet!");
         }
 
 
@@ -215,8 +303,11 @@ namespace BH.Adapter.ETABS
 
         #region Panel Results
 
-        public static List<IResult> GetPanelForce(cSapModel model, IList ids = null, IList cases = null, int divisions = 5)
+        public static List<PanelForce> GetPanelForce(cSapModel model, IList ids = null, IList cases = null, int divisions = 5)
         {
+
+
+
 
             return new List<IResult>();
         }
