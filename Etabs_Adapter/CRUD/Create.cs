7077960428836858b11f411fe6f2ef732fed7ef2
Josef Taylor <Josef.Taylor@burohappold.com>
2018-10-03 10:09:29 -0400

    Conform to BHoM change PanelResult -> MeshResult
    Keeping up with the Joneses (more accurately, the Mays).
    Responding to BHoM PR 285
    https://github.com/BuroHappoldEngineering/BHoM/pull/285

--- Etabs_Adapter/CRUD/Create.cs
+++ Etabs_Adapter/CRUD/Create.cs
@@ -1,18 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using BH.Engine.ETABS;
-using BH.oM;
-using BH.oM.Structure;
 using BH.oM.Structure.Elements;
 using BH.oM.Structure.Properties;
 using BH.oM.Structure.Loads;
 using BH.Engine.Structure;
 using BH.Engine.Geometry;
 using BH.oM.Common.Materials;
-using BH.Engine.Structure;
 
 namespace BH.Adapter.ETABS
 {
