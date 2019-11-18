e4a1095a6cdf3617edd8eb079965e539aad13abf
Isak Naslund <Isak.Naslund@burohappold.com>
2019-11-18 18:52:36 +0100

    First pass migration of MeshResults

--- Etabs_Adapter/Read/Result.cs
+++ Etabs_Adapter/Read/Result.cs
@@ -84,7 +84,7 @@ namespace BH.Adapter.ETABS
             else if (typeof(BarResult).IsAssignableFrom(type))
                 results = GetBarResults(type, ids, cases, divisions);
             else if (typeof(MeshElementResult).IsAssignableFrom(type))
-                results = GetMeshResults(type, ids, cases, divisions);
+                results = GetMeshResults(type, ids, cases);
             //else
             //    return new List<IResult>();
 
@@ -131,14 +131,14 @@ namespace BH.Adapter.ETABS
 
         /***************************************************/
 
-        private IEnumerable<IResult> GetMeshResults(Type type, IList ids = null, IList cases = null, int divisions = 5)
+        private IEnumerable<IResult> GetMeshResults(Type type, IList ids = null, IList cases = null)
         {
             IEnumerable<MeshElementResult> results = new List<MeshElementResult>();
 
             if (type == typeof(MeshForce))
-                results = GetMeshForce(ids, cases, divisions);
+                results = ReadMeshForce(ids, cases);
             else if (type == typeof(MeshStress))
-                results = GetMeshStress(ids, cases, divisions);
+                results = ReadMeshStress(ids, cases);
 
             return results;
         }
@@ -307,8 +307,6 @@ namespace BH.Adapter.ETABS
         /***************************************************/
 
 
-
-
         private List<PierForce> GetPierForce(IList ids = null, IList cases = null, int divisions = 5)
         {
             List<string> loadcaseIds = new List<string>();
@@ -367,152 +365,6 @@ namespace BH.Adapter.ETABS
         
 
 
-        private List<MeshForce> GetMeshForce(IList ids = null, IList cases = null, int divisions = 5)
-        {
-            List<string> loadcaseIds = new List<string>();
-            List<string> panelIds = new List<string>();
-            List<MeshForce> meshForces = new List<MeshForce>();
-
-            if (ids == null)
-            {
-                int panels = 0;
-                string[] names = null;
-                m_model.AreaObj.GetNameList(ref panels, ref names);
-                panelIds = names.ToList();
-            }
-            else
-            {
-                for (int i = 0; i < ids.Count; i++)
-                {
-                    panelIds.Add(ids[i].ToString());
-                }
-            }
-
-            //Gets and setup all the loadcases. if cases are null or have could 0, all are assigned
-            loadcaseIds = CheckAndSetUpCases(cases);
-            
-            eItemTypeElm ItemTypeElm = eItemTypeElm.ObjectElm;
-	        int resultCount = 0;
-	        string[] Obj = null;
-	        string[] Elm = null;
-            string[] PointElm = null;
-            string[] LoadCase = null;
-            string[] StepType = null;
-            double[] StepNum = null;
-            double[] F11 = null;
-            double[] F22 = null;
-            double[] F12 = null;
-            double[] FMax = null;
-            double[] FMin = null;
-            double[] FAngle = null;
-            double[] FVM = null;
-            double[] M11 = null;
-            double[] M22 = null;
-            double[] M12 = null;
-            double[] MMax = null;
-            double[] MMin = null;
-            double[] MAngle = null;
-            double[] V13 = null;
-            double[] V23 = null;
-            double[] VMax = null;
-            double[] VAngle = null;
-
-            for (int i = 0; i < panelIds.Count; i++)
-            {
-                
-                int ret = m_model.Results.AreaForceShell(panelIds[i], ItemTypeElm, ref resultCount, ref Obj, ref Elm,
-                    ref PointElm, ref LoadCase, ref StepType, ref StepNum, ref F11, ref F22, ref F12, ref FMax, ref FMin, ref FAngle, ref FVM,
-                    ref M11, ref M22, ref M12, ref MMax, ref MMin, ref MAngle, ref V13, ref V23, ref VMax, ref VAngle);
-
-                for (int j = 0; j < resultCount; j++)
-                {
-                    MeshForce pf = new MeshForce(panelIds[i], PointElm[j], Elm[i], LoadCase[j], StepNum[j], 0, 0, 0, 
-                        oM.Geometry.Basis.XY, F11[j], F22[j], F12[j], M12[j], M22[j], M12[j], V13[j], V23[j]);
-
-                    meshForces.Add(pf);
-                }
-            }
-
-            return meshForces;
-        }
-
-        /***************************************************/
-
-        private List<MeshStress> GetMeshStress(IList ids = null, IList cases = null, int divisions = 5)
-        {
-
-            List<string> loadcaseIds = new List<string>();
-            List<string> panelIds = new List<string>();
-            List<MeshStress> meshStresses = new List<MeshStress>();
-
-            if (ids == null)
-            {
-                int panels = 0;
-                string[] names = null;
-                m_model.AreaObj.GetNameList(ref panels, ref names);
-                panelIds = names.ToList();
-            }
-            else
-            {
-                for (int i = 0; i < ids.Count; i++)
-                {
-                    panelIds.Add(ids[i].ToString());
-                }
-            }
-
-            //Gets and setup all the loadcases. if cases are null or have could 0, all are assigned
-            loadcaseIds = CheckAndSetUpCases(cases);
-            
-            eItemTypeElm ItemTypeElm = eItemTypeElm.ObjectElm;
-            int resultCount = 0;
-            string[] obj = null;
-            string[] elm = null;
-            string[] pointElm = null;
-            string[] loadCase = null;
-            string[] stepType = null;
-            double[] stepNum = null;
-            double[] s11Top = null;
-            double[] s22Top = null;
-            double[] s12Top = null;
-            double[] sMaxTop = null;
-            double[] sMinTop = null;
-            double[] sAngTop = null;
-            double[] svmTop = null;
-            double[] s11Bot = null;
-            double[] s22Bot = null;
-            double[] s12Bot = null;
-            double[] sMaxBot = null;
-            double[] sMinBot = null;
-            double[] sAngBot = null;
-            double[] svmBot = null;
-            double[] s13Avg = null;
-            double[] s23Avg = null;
-            double[] sMaxAvg = null;
-            double[] sAngAvg = null;
-
-            
-            for (int i = 0; i < panelIds.Count; i++)
-            {
-                int ret = m_model.Results.AreaStressShell(panelIds[i], ItemTypeElm, ref resultCount, ref obj, ref elm, ref pointElm, ref loadCase, ref stepType, ref stepNum, ref s11Top, ref s22Top, ref s12Top, ref sMaxTop, ref sMinTop, ref sAngTop, ref svmTop, ref s11Bot, ref s22Bot, ref s12Bot, ref sMaxBot, ref sMinBot, ref sAngBot, ref svmBot, ref s13Avg, ref s23Avg, ref sMaxAvg, ref sAngAvg);
-                
-                if (ret == 0)
-                {
-                    for (int j = 0; j < resultCount; j++)
-                    {
-                        MeshStress mStressTop = new MeshStress(panelIds[i], pointElm[j], elm[j], loadCase[j], stepNum[j], MeshResultLayer.Upper, 1, MeshResultSmoothingType.None, oM.Geometry.Basis.XY, s11Top[j], s22Top[j], s12Top[j], s13Avg[j], s23Avg[j], sMaxTop[j], sMinTop[j], sMaxAvg[j]);
-                        MeshStress mStressBot = new MeshStress(panelIds[i], pointElm[j], elm[j], loadCase[j], stepNum[j], MeshResultLayer.Lower, 0, MeshResultSmoothingType.None, oM.Geometry.Basis.XY, s11Bot[j], s22Bot[j], s12Bot[j], s13Avg[j], s23Avg[j], sMaxBot[j], sMinBot[j], sMaxAvg[j]);
-
-                        meshStresses.Add(mStressBot);
-                        meshStresses.Add(mStressTop);
-                    }
-
-                }
-            }
-
-            return meshStresses;
-        }
-
-        /***************************************************/
         
         private List<GlobalReactions> GetGlobalReactions(IList cases = null)
         {
---
+++ Etabs_Adapter/Read/Results/MeshResults.cs
@@ -0,0 +1,239 @@
+﻿/*
+ * This file is part of the Buildings and Habitats object Model (BHoM)
+ * Copyright (c) 2015 - 2018, the respective contributors. All rights reserved.
+ *
+ * Each contributor holds copyright over their respective contributions.
+ * The project versioning (Git) records all such contribution source information.
+ *                                           
+ *                                                                              
+ * The BHoM is free software: you can redistribute it and/or modify         
+ * it under the terms of the GNU Lesser General Public License as published by  
+ * the Free Software Foundation, either version 3.0 of the License, or          
+ * (at your option) any later version.                                          
+ *                                                                              
+ * The BHoM is distributed in the hope that it will be useful,              
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of               
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the                 
+ * GNU Lesser General Public License for more details.                          
+ *                                                                            
+ * You should have received a copy of the GNU Lesser General Public License     
+ * along with this code. If not, see <https://www.gnu.org/licenses/lgpl-3.0.html>.      
+ */
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BH.oM.Structure.Results;
+using BH.oM.Common;
+#if Debug17 || Release17
+using ETABSv17;
+#else
+using ETABS2016;
+#endif
+using BH.oM.Structure.Elements;
+using BH.oM.Adapters.ETABS.Elements;
+using BH.Engine.ETABS;
+using BH.oM.Structure.Loads;
+using BH.oM.Structure.Requests;
+using BH.oM.Geometry;
+using BH.Engine.Geometry;
+
+namespace BH.Adapter.ETABS
+{
+#if Debug17 || Release17
+    public partial class ETABS17Adapter : BHoMAdapter
+#else
+    public partial class ETABS2016Adapter : BHoMAdapter
+#endif
+    {
+        /***************************************************/
+        /**** Public method - Read override             ****/
+        /***************************************************/
+
+        public IEnumerable<IResult> ReadResults(MeshResultRequest request)
+        {
+
+            switch (request.ResultType)
+            {
+                case MeshResultType.Forces:
+                    return ReadMeshForce(request.ObjectIds, request.Cases)
+                    break;
+                case MeshResultType.Stresses:
+                    break;
+                case MeshResultType.Displacements:
+                    break;
+                case MeshResultType.VonMises:
+                    break;
+                default:
+                    break;
+            }
+
+            switch (request.ResultType)
+            {
+                case B:
+                    return ReadBarForce(request.ObjectIds, request.Cases, request.Divisions);
+                case BarResultType.BarDeformation:
+                case BarResultType.BarStress:
+                case BarResultType.BarStrain:
+                default:
+                    Engine.Reflection.Compute.RecordError("Result extraction of type " + request.ResultType + " is not yet supported");
+                    return new List<IResult>();
+            }
+        }
+
+        /***************************************************/
+        /**** Private method - Extraction methods       ****/
+        /***************************************************/
+
+
+        private List<MeshForce> ReadMeshForce(IList ids = null, IList cases = null)
+        {
+            List<string> loadcaseIds = new List<string>();
+            List<string> panelIds = new List<string>();
+            List<MeshForce> meshForces = new List<MeshForce>();
+
+            if (ids == null)
+            {
+                int panels = 0;
+                string[] names = null;
+                m_model.AreaObj.GetNameList(ref panels, ref names);
+                panelIds = names.ToList();
+            }
+            else
+            {
+                for (int i = 0; i < ids.Count; i++)
+                {
+                    panelIds.Add(ids[i].ToString());
+                }
+            }
+
+            //Gets and setup all the loadcases. if cases are null or have could 0, all are assigned
+            loadcaseIds = CheckAndSetUpCases(cases);
+
+            eItemTypeElm ItemTypeElm = eItemTypeElm.ObjectElm;
+            int resultCount = 0;
+            string[] Obj = null;
+            string[] Elm = null;
+            string[] PointElm = null;
+            string[] LoadCase = null;
+            string[] StepType = null;
+            double[] StepNum = null;
+            double[] F11 = null;
+            double[] F22 = null;
+            double[] F12 = null;
+            double[] FMax = null;
+            double[] FMin = null;
+            double[] FAngle = null;
+            double[] FVM = null;
+            double[] M11 = null;
+            double[] M22 = null;
+            double[] M12 = null;
+            double[] MMax = null;
+            double[] MMin = null;
+            double[] MAngle = null;
+            double[] V13 = null;
+            double[] V23 = null;
+            double[] VMax = null;
+            double[] VAngle = null;
+
+            for (int i = 0; i < panelIds.Count; i++)
+            {
+                int ret = m_model.Results.AreaForceShell(panelIds[i], ItemTypeElm, ref resultCount, ref Obj, ref Elm,
+                    ref PointElm, ref LoadCase, ref StepType, ref StepNum, ref F11, ref F22, ref F12, ref FMax, ref FMin, ref FAngle, ref FVM,
+                    ref M11, ref M22, ref M12, ref MMax, ref MMin, ref MAngle, ref V13, ref V23, ref VMax, ref VAngle);
+
+                for (int j = 0; j < resultCount; j++)
+                {
+                    MeshForce pf = new MeshForce(panelIds[i], PointElm[j], Elm[i], LoadCase[j], StepNum[j], 0, 0, 0,
+                        oM.Geometry.Basis.XY, F11[j], F22[j], F12[j], M12[j], M22[j], M12[j], V13[j], V23[j]);
+
+                    meshForces.Add(pf);
+                }
+            }
+
+            return meshForces;
+        }
+
+        /***************************************************/
+
+        private List<MeshStress> ReadMeshStress(IList ids = null, IList cases = null)
+        {
+
+            List<string> loadcaseIds = new List<string>();
+            List<string> panelIds = new List<string>();
+            List<MeshStress> meshStresses = new List<MeshStress>();
+
+            if (ids == null)
+            {
+                int panels = 0;
+                string[] names = null;
+                m_model.AreaObj.GetNameList(ref panels, ref names);
+                panelIds = names.ToList();
+            }
+            else
+            {
+                for (int i = 0; i < ids.Count; i++)
+                {
+                    panelIds.Add(ids[i].ToString());
+                }
+            }
+
+            //Gets and setup all the loadcases. if cases are null or have could 0, all are assigned
+            loadcaseIds = CheckAndSetUpCases(cases);
+
+            eItemTypeElm ItemTypeElm = eItemTypeElm.ObjectElm;
+            int resultCount = 0;
+            string[] obj = null;
+            string[] elm = null;
+            string[] pointElm = null;
+            string[] loadCase = null;
+            string[] stepType = null;
+            double[] stepNum = null;
+            double[] s11Top = null;
+            double[] s22Top = null;
+            double[] s12Top = null;
+            double[] sMaxTop = null;
+            double[] sMinTop = null;
+            double[] sAngTop = null;
+            double[] svmTop = null;
+            double[] s11Bot = null;
+            double[] s22Bot = null;
+            double[] s12Bot = null;
+            double[] sMaxBot = null;
+            double[] sMinBot = null;
+            double[] sAngBot = null;
+            double[] svmBot = null;
+            double[] s13Avg = null;
+            double[] s23Avg = null;
+            double[] sMaxAvg = null;
+            double[] sAngAvg = null;
+
+
+            for (int i = 0; i < panelIds.Count; i++)
+            {
+                int ret = m_model.Results.AreaStressShell(panelIds[i], ItemTypeElm, ref resultCount, ref obj, ref elm, ref pointElm, ref loadCase, ref stepType, ref stepNum, ref s11Top, ref s22Top, ref s12Top, ref sMaxTop, ref sMinTop, ref sAngTop, ref svmTop, ref s11Bot, ref s22Bot, ref s12Bot, ref sMaxBot, ref sMinBot, ref sAngBot, ref svmBot, ref s13Avg, ref s23Avg, ref sMaxAvg, ref sAngAvg);
+
+                if (ret == 0)
+                {
+                    for (int j = 0; j < resultCount; j++)
+                    {
+                        MeshStress mStressTop = new MeshStress(panelIds[i], pointElm[j], elm[j], loadCase[j], stepNum[j], MeshResultLayer.Upper, 1, MeshResultSmoothingType.None, oM.Geometry.Basis.XY, s11Top[j], s22Top[j], s12Top[j], s13Avg[j], s23Avg[j], sMaxTop[j], sMinTop[j], sMaxAvg[j]);
+                        MeshStress mStressBot = new MeshStress(panelIds[i], pointElm[j], elm[j], loadCase[j], stepNum[j], MeshResultLayer.Lower, 0, MeshResultSmoothingType.None, oM.Geometry.Basis.XY, s11Bot[j], s22Bot[j], s12Bot[j], s13Avg[j], s23Avg[j], sMaxBot[j], sMinBot[j], sMaxAvg[j]);
+                        
+                        meshStresses.Add(mStressBot);
+                        meshStresses.Add(mStressTop);
+                    }
+
+                }
+            }
+
+            return meshStresses;
+        }
+
+        /***************************************************/
+
+    }
+
