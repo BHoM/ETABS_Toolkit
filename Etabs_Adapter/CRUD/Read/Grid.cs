0ce256db96b1f5696de6ff41441efef9cb58be7a
kThorsager <Kalle.Thorsager@BuroHappold.com>
2020-03-05 11:20:09 +0000

    Re enable grids

--- Etabs_Adapter/CRUD/Read/Grid.cs
+++ Etabs_Adapter/CRUD/Read/Grid.cs
@@ -57,8 +57,21 @@ namespace BH.Adapter.ETABS
 
             foreach (string id in gridSystemId)
             {
+                string gridSysType = "";
                 // Each GridSystem has an Id in ETABS
-                gridList.AddRange(CartesianGrids(id, ids));
+                m_model.GridSys.GetGridSysType(id, ref gridSysType);
+                switch (gridSysType)
+                {
+                    case "Cartesian":
+                        gridList.AddRange(CartesianGrids(id, ids));
+                        break;
+                    case "Cylindrical":
+                        gridList.AddRange(CylindricalGrids(id, ids));
+                        break;
+                    default:
+                        Engine.Reflection.Compute.RecordWarning("Can not pull " + gridSysType + " Grid systems from ETABS, offender: " + id);
+                        break;
+                }
             }
 
             return gridList;
@@ -72,25 +85,43 @@ namespace BH.Adapter.ETABS
             double xO = 0;
             double yO = 0;
             double rZ = 0;
+
+            bool storyRangedefault = true;
+            string top = "";
+            string bottom = "";
+            double bubbleSize = 0;
+            int gridColour = -1;
+
             int numXLines = 0;
             string[] gridLineIDX = null;
             double[] ordinateX = null;
             bool[] visibleX = null;
             string[] bubbleLocX = null;
+
             int numYLines = 0;
             string[] gridLineIDY = null;
             double[] ordinateY = null;
             bool[] visibleY = null;
             string[] bubbleLocY = null;
-            string gridSysType = "";
 
-            if (m_model.GridSys.GetGridSys_2(
-                id, ref xO, ref yO, ref rZ, ref gridSysType,
-                ref numXLines, ref numYLines,
-                ref gridLineIDX, ref gridLineIDY,
-                ref ordinateX, ref ordinateY,
-                ref visibleX, ref visibleY,
-                ref bubbleLocX, ref bubbleLocY) == 1)
+            int numGLines = 0;
+            string[] gridLineIDG = null;
+            double[] ordinateGX1 = null;
+            double[] ordinateGY1 = null;
+            double[] ordinateGX2 = null;
+            double[] ordinateGY2 = null;
+            bool[] visibleG = null;
+            string[] bubbleLocG = null;
+
+            if (m_model.GridSys.GetGridSysCartesian(
+                id, ref xO, ref yO, ref rZ,
+                ref storyRangedefault, ref top, ref bottom,
+                ref bubbleSize, ref gridColour,
+                ref numXLines, ref gridLineIDX, ref ordinateX, ref visibleX, ref bubbleLocX,
+                ref numYLines, ref gridLineIDY, ref ordinateY, ref visibleY, ref bubbleLocY,
+                ref numGLines, ref gridLineIDG,
+                    ref ordinateGX1, ref ordinateGY1, ref ordinateGX2, ref ordinateGY2,
+                ref visibleG, ref bubbleLocG) == 1)
             {
                 return new List<Grid>();
             }
@@ -102,88 +133,135 @@ namespace BH.Adapter.ETABS
             double minX = ordinateX.Min();
             double maxX = ordinateX.Max();
 
+            // Cull based on ids if present
+            if (ids != null)
+            {
+                ordinateX = ordinateX.Where((value, i) => ids.Contains(gridLineIDX[i])).ToArray();
+                ordinateY = ordinateY.Where((value, i) => ids.Contains(gridLineIDY[i])).ToArray();
+                gridLineIDX = gridLineIDX.Where(x => ids.Contains(x)).ToArray();
+                gridLineIDY = gridLineIDY.Where(x => ids.Contains(x)).ToArray();
+
+                ordinateGX1 = ordinateGX1.Where((value, i) => ids.Contains(gridLineIDG[i])).ToArray();
+                ordinateGY1 = ordinateGY1.Where((value, i) => ids.Contains(gridLineIDG[i])).ToArray();
+                ordinateGX2 = ordinateGX2.Where((value, i) => ids.Contains(gridLineIDG[i])).ToArray();
+                ordinateGY2 = ordinateGY2.Where((value, i) => ids.Contains(gridLineIDG[i])).ToArray();
+                gridLineIDG = gridLineIDG.Where(x => ids.Contains(x)).ToArray();
+            }
+
             // Format the names like the Lines
-            List<string> names = gridLineIDX.Concat(gridLineIDY).ToList();
+            List<string> names = gridLineIDX.Concat(gridLineIDY).Concat(gridLineIDG).ToList();
 
-            if (gridSysType == "Cartesian")
+            // Create Lines in each orientation
+            List<Line> result = ordinateX.Select(x => new Line()
             {
-                // Cull based on ids if present
-                if (ids != null)
-                {
-                    ordinateX = ordinateX.Where((value, i) => ids.Contains(gridLineIDX[i])).ToArray();
-                    ordinateY = ordinateY.Where((value, i) => ids.Contains(gridLineIDY[i])).ToArray();
-                    gridLineIDX = gridLineIDX.Where(x => ids.Contains(x)).ToArray();
-                    gridLineIDY = gridLineIDY.Where(x => ids.Contains(x)).ToArray();
-                }
-
-                // Create Lines in each orientation
-                List<Line> result = ordinateX.Select(x => new Line()
-                {
-                    Start = new Point() { X = x, Y = minY },
-                    End = new Point() { X = x, Y = maxY }
-                }
-                ).ToList();
-                result.AddRange(ordinateY.Select(y => new Line()
-                {
-                    Start = new Point() { X = minX, Y = y },
-                    End = new Point() { X = maxX, Y = y }
-                }
-                ));
-
-                // Place at gridsystem origin and orientation
-                result = result.Select(x => Engine.Geometry.Modify.Rotate(x, Point.Origin, Vector.ZAxis, rZ * Math.PI / 180)).ToList();
-                Vector origin = new Vector() { X = xO, Y = yO };
-                result = result.Select(x => Engine.Geometry.Modify.Translate(x, origin)).ToList();
-
-                return result.Zip(names, (x, name) => new Grid() { Curve = x, Name = name }).ToList();
-
-            } else if (gridSysType == "Cylindrical")
+                Start = new Point() { X = x, Y = minY },
+                End = new Point() { X = x, Y = maxY }
+            }
+            ).ToList();
+            result.AddRange(ordinateY.Select(y => new Line()
             {
-                Engine.Reflection.Compute.RecordWarning("Can not pull " + gridSysType + " Grid systems from ETABS due to API limitations, offender: " + id);
-                return new List<Grid>();
-                // ETABS scales the angle of the grids in the gridsystem by length convertion units, and until such a time as they stop doing that
-                // Cylindrical grids will not be implemented
+                Start = new Point() { X = minX, Y = y },
+                End = new Point() { X = maxX, Y = y }
+            }
+            ));
+            result.AddRange(gridLineIDG.Select((identificator, index) =>
+           {
+               return new Line()
+               {
+                   Start = new Point() { X = ordinateGX1[index], Y = ordinateGY1[index] },
+                   End = new Point() { X = ordinateGX2[index], Y = ordinateGY2[index] }
+               };
+           }));
 
-                double radianFactor = Math.PI / (180 * 0.0254); // This works for ETABS models in inches, but not for others...
-                // this is the only thing that would need change once ETABS API actually returns angles in radians or degrees consistently (or at all)
+            // Place at gridsystem origin and orientation
+            result = result.Select(x => Engine.Geometry.Modify.Rotate(x, Point.Origin, Vector.ZAxis, rZ * Math.PI / 180)).ToList();
+            Vector origin = new Vector() { X = xO, Y = yO };
+            result = result.Select(x => Engine.Geometry.Modify.Translate(x, origin)).ToList();
 
-                List<ICurve> result = ordinateX.Select(r =>
-                {
-                    List<Point> cPoints = new List<Point>();
-                    foreach (double radians in ordinateY)
-                    {
-                        Point dir = (new Point() { X = 1 }).Rotate(Point.Origin, Vector.ZAxis, radians * radianFactor);
-                        cPoints.Add(dir * r);
-                    }
-                    return new Polyline() { ControlPoints = cPoints };
-                }
-                ).ToList<ICurve>();
-                result.AddRange(ordinateY.Select(radians =>
-                {
-                    Point dir = (new Point() { X = 1 }).Rotate(Point.Origin, Vector.ZAxis, radians * radianFactor);
-                    return new Line()
-                    {
-                        Start = dir * minX,
-                        End = dir * maxX
-                    };
-                }
-                ));
+            return result.Zip(names, (x, name) => new Grid() { Curve = x, Name = name }).ToList();
 
-                // Place at gridsystem origin and orientation
-                result = result.Select(x => Engine.Geometry.Modify.IRotate(x, Point.Origin, Vector.ZAxis, rZ * Math.PI / 180)).ToList();
-                Vector origin = new Vector() { X = xO, Y = yO };
-                result = result.Select(x => Engine.Geometry.Modify.ITranslate(x, origin)).ToList();
 
-                if (ids != null)
-                    return result.Zip(names, (x, name) => new Grid() { Curve = x, Name = name }).Where(grid => ids.Contains(grid.Name)).ToList();
-                else
-                    return result.Zip(names, (x, name) => new Grid() { Curve = x, Name = name }).ToList();
+        }
 
-            } else
+        /***************************************************/
+
+        [Description("Gets all the grids in a Cylindrical Grid System")]
+        private List<Grid> CylindricalGrids(string id, List<string> ids)
+        {
+            double xO = 0;
+            double yO = 0;
+            double rZ = 0;
+
+            bool storyRangeDefault = true;
+            string top = "";
+            string bottom = "";
+
+            double bubbleSize = 0;
+            int gridColour = -1;
+
+            int numRLines = 0;
+            string[] gridLineIDR = null;
+            double[] ordinateR = null;
+            bool[] visibleR = null;
+            string[] bubbleLocR = null;
+
+            int numTLines = 0;
+            string[] gridLineIDT = null;
+            double[] ordinateT = null;
+            bool[] visibleT = null;
+            string[] bubbleLocT = null;
+
+            if (m_model.GridSys.GetGridSysCylindrical(
+                id, ref xO, ref yO, ref rZ,
+                ref storyRangeDefault, ref top, ref bottom,
+                ref bubbleSize, ref gridColour,
+                ref numRLines, ref gridLineIDR, ref ordinateR, ref visibleR, ref bubbleLocR,
+                ref numTLines, ref gridLineIDT, ref ordinateT, ref visibleT, ref bubbleLocT) == 1)
             {
-                Engine.Reflection.Compute.RecordWarning("Can not pull " + gridSysType + " Grid systems from ETABS, offender: " + id);
                 return new List<Grid>();
             }
+
+            // Get bounds
+            double minX = ordinateR.Min();
+            double maxX = ordinateR.Max();
+
+            // Format the names like the Lines
+            List<string> names = gridLineIDR.Concat(gridLineIDT).ToList();
+            
+            // Create the Lines
+            List<ICurve> result = ordinateR.Select(r =>
+            {
+                List<Point> cPoints = new List<Point>();
+                foreach (double radians in ordinateT)
+                {
+                    Point dir = (new Point() { X = 1 }).Rotate(Point.Origin, Vector.ZAxis, radians * Math.PI / 180);
+                    cPoints.Add(dir * r);
+                }
+                return new Polyline() { ControlPoints = cPoints };
+            }
+            ).ToList<ICurve>();
+            result.AddRange(ordinateT.Select(radians =>
+            {
+                Point dir = (new Point() { X = 1 }).Rotate(Point.Origin, Vector.ZAxis, radians * Math.PI / 180);
+                return new Line()
+                {
+                    Start = dir * minX,
+                    End = dir * maxX
+                };
+            }
+            ));
+
+            // Place at gridsystem origin and orientation
+            result = result.Select(x => Engine.Geometry.Modify.IRotate(x, Point.Origin, Vector.ZAxis, rZ * Math.PI / 180)).ToList();
+            Vector origin = new Vector() { X = xO, Y = yO };
+            result = result.Select(x => Engine.Geometry.Modify.ITranslate(x, origin)).ToList();
+
+            // Remove un-requested grids if needed
+            if (ids != null)
+                return result.Zip(names, (x, name) => new Grid() { Curve = x, Name = name }).Where(grid => ids.Contains(grid.Name)).ToList();
+            else
+                return result.Zip(names, (x, name) => new Grid() { Curve = x, Name = name }).ToList();
+
         }
 
         /***************************************************/
