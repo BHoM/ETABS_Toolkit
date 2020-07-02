fc99fcb0ad19c395829cac55878ad9ed332d90f8
Kalle Thorsager <Kalle.Thorsager@BuroHappold.com>
2020-07-02 10:02:47 +0100

    Add Delete

--- Etabs_Adapter/CRUD/Delete/_Delete.cs
+++ Etabs_Adapter/CRUD/Delete/_Delete.cs
@@ -26,6 +26,15 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using BH.oM.Adapter;
+using BH.oM.Structure.Elements;
+using BH.oM.Structure.SectionProperties;
+using BH.oM.Structure.MaterialFragments;
+using BH.oM.Structure.SurfaceProperties;
+using BH.oM.Structure.Loads;
+using BH.oM.Structure.Constraints;
+using BH.oM.Base;
+using BH.oM.Data.Requests;
+using System.Collections;
 
 namespace BH.Adapter.ETABS
 {
@@ -43,7 +52,90 @@ namespace BH.Adapter.ETABS
 
         protected override int IDelete(Type type, IEnumerable<object> ids, ActionConfig actionConfig = null)
         {
-            return base.IDelete(type, ids, actionConfig);
+            List<string> listIds = new List<string>();
+            if (ids != null)
+                listIds = ids.Cast<string>().ToList();
+
+            int count = listIds.Count;
+            int controlCount = count;
+
+            if (type == typeof(Node))
+                foreach (string id in listIds)
+                    count -= m_model.PointObj.DeleteSpecialPoint(id);
+            else if (type == typeof(Bar))
+                foreach (string id in listIds)
+                    count -= m_model.FrameObj.Delete(id);
+            else if (type == typeof(ISectionProperty) || type.GetInterfaces().Contains(typeof(ISectionProperty)))
+                foreach (string id in listIds)
+                    count -= m_model.PropFrame.Delete(id);
+            else if (type == typeof(IMaterialFragment))
+                foreach (string id in listIds)
+                    count -= m_model.PropMaterial.Delete(id);
+            else if (type == typeof(Panel))
+                foreach (string id in listIds)
+                    count -= m_model.AreaObj.Delete(id);
+            else if (type == typeof(ISurfaceProperty))
+                foreach (string id in listIds)
+                    count -= m_model.PropArea.Delete(id);
+            else if (type == typeof(LoadCombination))
+                foreach (string id in listIds)
+                    count -= m_model.LoadPatterns.Delete(id);
+            else if (type == typeof(Loadcase))
+                foreach (string id in listIds)
+                    count -= m_model.LoadCases.Delete(id);
+            else if (type == typeof(ILoad) || type.GetInterfaces().Contains(typeof(ILoad)))
+                Engine.Reflection.Compute.RecordError("Loads do not have ids in ETABS, try deleting Loadcases, LoadCombinations or overwriting the old loads with `ReplaceLoads`.");
+            else if (type == typeof(RigidLink))
+                foreach (string id in listIds)
+                    count -= m_model.LinkObj.Delete(id);
+            else if (type == typeof(LinkConstraint))
+                foreach (string id in listIds)
+                    count -= m_model.PropLink.Delete(id);
+            else if (type == typeof(oM.Geometry.SettingOut.Level) || type == typeof(oM.Architecture.Elements.Level))
+                Engine.Reflection.Compute.RecordError("Can't delete levels in ETABS.");
+            else if (type == typeof(oM.Geometry.SettingOut.Grid))
+                Engine.Reflection.Compute.RecordError("Can't delete grids in ETABS.");
+            else if (type == typeof(FEMesh))
+                foreach (string id in listIds)
+                    count -= m_model.AreaObj.Delete(id);
+
+            if (count != controlCount)
+            {
+                Engine.Reflection.Compute.RecordWarning("Some of the requested delete operations failed.");
+            }
+
+            return count;
+        }
+
+        /***************************************************/
+
+        protected override int Delete(IRequest request, ActionConfig actionConfig = null)
+        {
+            if (request is SelectionRequest)
+            {
+                int count = 0;
+
+                foreach (KeyValuePair<Type, List<string>> keyVal in SelectedElements())
+                {
+                    count += IDelete(keyVal.Key, keyVal.Value, actionConfig);
+                }
+
+                return count;
+            }
+
+            // Temporary fix until BHoM_Adpter works
+            if (request is FilterRequest)
+            {
+                List<string> objectIds = null;
+                object idObject;
+                if ((request as FilterRequest).Equalities.TryGetValue("ObjectIds", out idObject) && idObject is List<string>)
+                    objectIds = idObject as List<string>;
+
+                // Call the Basic Method Read() to get the objects based on the Ids
+                return IDelete((request as FilterRequest).Type, objectIds, actionConfig);
+            }
+
+            return base.Delete(request, actionConfig);
         }
 
         /***************************************************/
