6a709ebfedcde7dc4e361a67cde23b31c19ac66b
Isak Naslund <Isak.Naslund@burohappold.com>
2020-10-16 09:31:59 +0200

    Aligning with changes to ShapeProfiles

--- Etabs_Adapter/CRUD/Read/SectionProperty.cs
+++ Etabs_Adapter/CRUD/Read/SectionProperty.cs
@@ -29,7 +29,7 @@ using System.Threading.Tasks;
 using BH.oM.Structure.SectionProperties;
 using BH.oM.Structure.MaterialFragments;
 using BH.oM.Geometry;
-using BH.oM.Geometry.ShapeProfiles;
+using BH.oM.Spatial.ShapeProfiles;
 using BH.Engine.Structure;
 using BH.oM.Structure.Fragments;
 #if Debug17 || Release17
@@ -102,43 +102,43 @@ namespace BH.Adapter.ETABS
                         case eFramePropType.I:
                             m_model.PropFrame.GetISection(id, ref fileName, ref materialName, ref t3, ref t2, ref tf, ref tw, ref t2b, ref tfb, ref colour, ref notes, ref guid);
                             if (t2 == t2b)
-                                dimensions = Engine.Geometry.Create.ISectionProfile(t3, t2, tw, tf, 0, 0);
+                                dimensions = Engine.Spatial.Create.ISectionProfile(t3, t2, tw, tf, 0, 0);
                             else
-                                dimensions = Engine.Geometry.Create.FabricatedISectionProfile(t3, t2, t2b, tw, tf, tfb, 0);
+                                dimensions = Engine.Spatial.Create.FabricatedISectionProfile(t3, t2, t2b, tw, tf, tfb, 0);
                             break;
                         case eFramePropType.Channel:
                             m_model.PropFrame.GetChannel(id, ref fileName, ref materialName, ref t3, ref t2, ref tf, ref tw, ref colour, ref notes, ref guid);
-                            dimensions = Engine.Geometry.Create.ChannelProfile(t3, t2, tw, tf, 0, 0);
+                            dimensions = Engine.Spatial.Create.ChannelProfile(t3, t2, tw, tf, 0, 0);
                             break;
                         case eFramePropType.T:
                             break;
                         case eFramePropType.Angle:
                             m_model.PropFrame.GetAngle(id, ref fileName, ref materialName, ref t3, ref t2, ref tf, ref tw, ref colour, ref notes, ref guid);
-                            dimensions = Engine.Geometry.Create.AngleProfile(t3, t2, tw, tf, 0, 0);
+                            dimensions = Engine.Spatial.Create.AngleProfile(t3, t2, tw, tf, 0, 0);
                             break;
                         case eFramePropType.DblAngle:
                             break;
                         case eFramePropType.Box:
                             m_model.PropFrame.GetTube(id, ref fileName, ref materialName, ref t3, ref t2, ref tf, ref tw, ref colour, ref notes, ref guid);
                             if (tf == tw)
-                                dimensions = Engine.Geometry.Create.BoxProfile(t3, t2, tf, 0, 0);
+                                dimensions = Engine.Spatial.Create.BoxProfile(t3, t2, tf, 0, 0);
                             else
-                                dimensions = Engine.Geometry.Create.FabricatedBoxProfile(t3, t2, tw, tf, tf, 0);
+                                dimensions = Engine.Spatial.Create.FabricatedBoxProfile(t3, t2, tw, tf, tf, 0);
                             break;
                         case eFramePropType.Pipe:
                             m_model.PropFrame.GetPipe(id, ref fileName, ref materialName, ref t3, ref tw, ref colour, ref notes, ref guid);
-                            dimensions = Engine.Geometry.Create.TubeProfile(t3, tw);
+                            dimensions = Engine.Spatial.Create.TubeProfile(t3, tw);
                             break;
                         case eFramePropType.Rectangular:
                             m_model.PropFrame.GetRectangle(id, ref fileName, ref materialName, ref t3, ref t2, ref colour, ref notes, ref guid);
-                            dimensions = Engine.Geometry.Create.RectangleProfile(t3, t2, 0);
+                            dimensions = Engine.Spatial.Create.RectangleProfile(t3, t2, 0);
                             break;
                         case eFramePropType.Auto://not member will have this assigned but it still exists in the propertyType list
-                            dimensions = Engine.Geometry.Create.CircleProfile(0.2);
+                            dimensions = Engine.Spatial.Create.CircleProfile(0.2);
                             break;
                         case eFramePropType.Circle:
                             m_model.PropFrame.GetCircle(id, ref fileName, ref materialName, ref t3, ref colour, ref notes, ref guid);
-                            dimensions = Engine.Geometry.Create.CircleProfile(t3);
+                            dimensions = Engine.Spatial.Create.CircleProfile(t3);
                             break;
                         case eFramePropType.General:
                             constructSelector = "explicit";
@@ -232,7 +232,7 @@ namespace BH.Adapter.ETABS
                             }
                             else
                             {
-                                dimensions = Engine.Geometry.Create.TaperedProfile(positions, profiles, interpolationOrder);
+                                dimensions = Engine.Spatial.Create.TaperedProfile(positions, profiles, interpolationOrder);
                             }
                         break;
                         case eFramePropType.Joist:
@@ -263,7 +263,7 @@ namespace BH.Adapter.ETABS
                             break;
                         case eFramePropType.Concrete_L:
                             m_model.PropFrame.GetConcreteL(id, ref fileName, ref materialName, ref t3, ref t2, ref tf, ref tw, ref twt, ref horFlip, ref verticalFlip, ref colour, ref notes, ref guid);
-                            dimensions = Engine.Geometry.Create.AngleProfile(t3, t2, tw, tf, 0, 0, horFlip, verticalFlip);
+                            dimensions = Engine.Spatial.Create.AngleProfile(t3, t2, tw, tf, 0, 0, horFlip, verticalFlip);
                             break;
                         case eFramePropType.FilledTube:
                             break;
@@ -279,28 +279,28 @@ namespace BH.Adapter.ETABS
                             break;
                         case eFramePropType.ConcreteTee:
                             m_model.PropFrame.GetConcreteTee(id, ref fileName, ref materialName, ref t3, ref t2, ref tf, ref tw, ref twt, ref verticalFlip, ref colour, ref notes, ref guid);
-                            dimensions = Engine.Geometry.Create.TSectionProfile(t3, t2, tw, tf, 0, 0, verticalFlip);
+                            dimensions = Engine.Spatial.Create.TSectionProfile(t3, t2, tw, tf, 0, 0, verticalFlip);
                             break;
                         case eFramePropType.ConcreteBox:
                             m_model.PropFrame.GetTube(id, ref fileName, ref materialName, ref t3, ref t2, ref tf, ref tw, ref colour, ref notes, ref guid);
                             if (tf == tw)
-                                dimensions = Engine.Geometry.Create.BoxProfile(t3, t2, tf, 0, 0);
+                                dimensions = Engine.Spatial.Create.BoxProfile(t3, t2, tf, 0, 0);
                             else
-                                dimensions = Engine.Geometry.Create.FabricatedBoxProfile(t3, t2, tw, tf, tf, 0);
+                                dimensions = Engine.Spatial.Create.FabricatedBoxProfile(t3, t2, tw, tf, tf, 0);
                             break;
                         case eFramePropType.ConcretePipe:
                             m_model.PropFrame.GetPipe(id, ref fileName, ref materialName, ref t3, ref tw, ref colour, ref notes, ref guid);
-                            dimensions = Engine.Geometry.Create.TubeProfile(t3, tw);
+                            dimensions = Engine.Spatial.Create.TubeProfile(t3, tw);
                             break;
                         case eFramePropType.ConcreteCross:
                             break;
                         case eFramePropType.SteelPlate:
                             m_model.PropFrame.GetPlate(id, ref fileName, ref materialName, ref t3, ref t2, ref colour, ref notes, ref guid);
-                            dimensions = Engine.Geometry.Create.RectangleProfile(t3, t2, 0);
+                            dimensions = Engine.Spatial.Create.RectangleProfile(t3, t2, 0);
                             break;
                         case eFramePropType.SteelRod:
                             m_model.PropFrame.GetRod(id, ref fileName, ref materialName, ref t3, ref colour, ref notes, ref guid);
-                            dimensions = Engine.Geometry.Create.CircleProfile(t3);
+                            dimensions = Engine.Spatial.Create.CircleProfile(t3);
                             break;
                         default:
                             break;
