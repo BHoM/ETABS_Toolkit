335cbf311528b552af0abe3fd08204c0f3cdc446
FraserGreenroyd <fraser.greenroyd@burohappold.com>
2022-07-18 17:38:03 +0100

    Update Assembly Information

--- Etabs_Adapter/Properties/AssemblyInfo.cs
+++ Etabs_Adapter/Properties/AssemblyInfo.cs
@@ -28,7 +28,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("ETABS_Adapter")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("https://github.com/BHoM/ETABS_Toolkit")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("ETABS_Adapter")]
