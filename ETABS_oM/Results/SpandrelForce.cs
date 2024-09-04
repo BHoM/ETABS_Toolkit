b09f44e069dd23de12e8b360f7ce2948ebf4e300
GCRA101 <giorgio.albieri@burohappold.com>
2024-09-04 10:04:01 +0100

    Add missing namespace references to SpandrelForce class

--- ETABS_oM/Results/SpandrelForce.cs
+++ ETABS_oM/Results/SpandrelForce.cs
@@ -21,12 +21,17 @@
  */
 
 
+
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using BH.oM.Structure.Results;
 using BH.oM.Base;
+using BH.oM.Quantities.Attributes;
+using System.ComponentModel;
+
 
 namespace BH.oM.Adapters.ETABS.Results
 {
