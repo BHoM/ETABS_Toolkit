33e00dceb04ecc1b56b6996b01cff03f59b736bf
GCRA101 <126593217+GCRA101@users.noreply.github.com>
2025-10-28 19:13:03 +0000

    Update Level.cs to work with Multiple Towers and pick up the Base Level
    The new code gets the list of Towers in the model, extracts the data regarding the base levels of the towers from the Database Tables of the models and return to the user all the levels beloning to all towers.

--- Etabs_Adapter/CRUD/Read/Level.cs
+++ Etabs_Adapter/CRUD/Read/Level.cs
@@ -30,6 +30,7 @@ using System.Text;
 using System.Threading.Tasks;
 using BH.oM.Spatial.SettingOut;
 using BH.Engine.Adapters.ETABS;
+using BH.oM.Base;
 
 
 namespace BH.Adapter.ETABS
@@ -47,28 +48,79 @@ namespace BH.Adapter.ETABS
         private List<Level> ReadLevel(List<string> ids = null)
         {
             List<Level> levellist = new List<Level>();
-            int numberNames = 0;
-            string[] names = null;
-            m_model.Story.GetNameList(ref numberNames, ref names);
 
-            ids = FilterIds(ids, names);
+            int towersNum = 0;
+            string[] towerNames = null;
+            m_model.Tower.GetNameList(ref towersNum, ref towerNames);
 
-            foreach (string id in ids)
+            // Prepare variables
+            string tableKey = "Tower and Base Story Definitions";
+            string[] fieldKeyList = null;
+            string groupName = "All";
+            int tableVersion = 0;
+            string[] fieldsKeysIncluded = null;
+            int numberRecords = 0;
+            string[] tableData = null;
+
+            // Get table data
+            m_model.DatabaseTables.GetTableForDisplayArray(tableKey, ref fieldKeyList, groupName, ref tableVersion, 
+                                                                 ref fieldsKeysIncluded, ref numberRecords, ref tableData);
+
+
+            double baseElevation = 0;
+            int numberStories = 0;
+            bool[] isMasterStory = null;
+            bool[] spliceAbove = null;
+            double[] storyElevations = null;
+            double[] storyHeights = null;
+            double[] spliceHeight = null;
+            string[] storyNames = null;
+            string[] similarToStory = null;
+            int[] color = null;
+
+            List<string> storyNamesList;
+            List<double> storyElevationsList;
+
+
+            for (int i = 0; i < towersNum; i++)
             {
-                ETABSId etabsid = new ETABSId();
-                etabsid.Id = id;
 
-                double elevation = 0;
-                int ret = m_model.Story.GetElevation(id, ref elevation);
+                m_model.Tower.SetActiveTower(towerNames[i]);
 
-                string guid = null;
-                m_model.Story.GetGUID(id, ref guid);
-                etabsid.PersistentId = guid;
+                m_model.Story.GetStories_2(ref baseElevation, ref numberStories, ref storyNames, ref storyElevations, ref storyHeights,
+                            ref isMasterStory, ref similarToStory, ref spliceAbove, ref spliceHeight, ref color);
 
-                Level lvl = new Level() { Elevation = elevation, Name = id };
+                int i_baseLevelName = fieldsKeysIncluded.ToList().IndexOf("BSName")+i*fieldsKeysIncluded.Count();
+                int i_baseLevelElev = fieldsKeysIncluded.ToList().IndexOf("BSElev")+i*fieldsKeysIncluded.Count();
+                string baseLevelName = tableData[i_baseLevelName];
+                double baseLevelElev = Double.Parse(tableData[i_baseLevelElev]);
+                
+                storyNamesList = storyNames.ToList();
+                storyNamesList.Insert(0, baseLevelName);
+                storyNames = storyNamesList.ToArray();
 
-                lvl.SetAdapterId(etabsid);
-                levellist.Add(lvl);
+                storyElevationsList = storyElevations.ToList();
+                storyElevationsList.Insert(0, baseLevelElev);
+                storyElevations = storyElevationsList.ToArray();
+
+                ids = FilterIds(ids, storyNames);
+
+                for (int j = 0; j < ids.Count; j++)
+                {
+                    ETABSId etabsid = new ETABSId();
+                    etabsid.Id = ids[j];
+
+                    string guid = null;
+                    m_model.Story.GetGUID(ids[j], ref guid);
+                    etabsid.PersistentId = guid;
+
+                    Level lvl = new Level() { Elevation = storyElevations[j], Name = ids[j] };
+                    lvl.Fragments.Add( new BH.oM.Adapters.ETABS.Elements.Tower  { Name = towerNames[i] } );
+
+                    lvl.SetAdapterId(etabsid);
+                    levellist.Add(lvl);
+                }
+
             }
 
             return levellist;
