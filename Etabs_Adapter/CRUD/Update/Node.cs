1bd48f623f699b75da1189f1590c2f3bd684fe2d
GCRA101 <126593217+GCRA101@users.noreply.github.com>
2025-11-24 15:48:15 +0000

    Add Assignment of UniqueName in Update of Nodes

--- Etabs_Adapter/CRUD/Update/Node.cs
+++ Etabs_Adapter/CRUD/Update/Node.cs
@@ -20,14 +20,19 @@
  * along with this code. If not, see <https://www.gnu.org/licenses/lgpl-3.0.html>.      
  */
 
-using System.Collections.Generic;
-using System.Linq;
 using BH.Engine.Adapter;
-using BH.oM.Adapters.ETABS;
-using BH.oM.Structure.Elements;
-using BH.oM.Structure.Constraints;
 using BH.Engine.Adapters.ETABS;
+using BH.Engine.Structure;
+using BH.oM.Adapters.ETABS;
 using BH.oM.Physical.Elements;
+using BH.oM.Structure.Constraints;
+using BH.oM.Structure.Elements;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Xml.Linq;
 
 namespace BH.Adapter.ETABS
 {
@@ -53,85 +58,124 @@ namespace BH.Adapter.ETABS
             Engine.Structure.NodeDistanceComparer comparer = AdapterComparers[typeof(Node)]    // θ(1)
                 as Engine.Structure.NodeDistanceComparer;
 
-            Dictionary<double, List<string>> dx = new Dictionary<double, List<string>>();      // θ(1)
-            Dictionary<double, List<string>> dy = new Dictionary<double, List<string>>();      // θ(1)
-            Dictionary<double, List<string>> dz = new Dictionary<double, List<string>>();      // θ(1)
-
-
             // 1. GROUP NODES BY RELATIVE MOVEMENT IN X/Y/Z DIRECTION  -  ** HASH TABLES **
 
             foreach (Node bhNode in nodes)                                                     // n*θ(1) + θ(1)
             {
                 string name = GetAdapterId<string>(bhNode);                                    // θ(1)
-
-                // Update position
-                double x = 0;                                                                  // θ(1)
-                double y = 0;                                                                  // θ(1)
-                double z = 0;                                                                  // θ(1)
-
-                if (m_model.PointObj.GetCoordCartesian(name, ref x, ref y, ref z) == 0)        // θ(1)
-                {
-                    oM.Geometry.Point p = new oM.Geometry.Point() { X = x, Y = y, Z = z };     // θ(1)
-
-                    if (!comparer.Equals(bhNode, (Node)p))                                     // θ(1)
-                    {
-                        // Get BHoM vs ETABS differences in nodes coordinates
-                        x = bhNode.Position.X - x;                                             // θ(1)
-                        y = bhNode.Position.Y - y;                                             // θ(1)
-                        z = bhNode.Position.Z - z;                                             // θ(1)
-
-                        // Add Node name and corresponding dX in dx Hash Table
-                        if (dx.ContainsKey(x)) dx[x].Add(name);                                // θ(1)
-                        else dx.Add(x, new List<string>() {name});                             // θ(1)
-                        // Add Node name and corresponding dY in dy Hash Table
-                        if (dy.ContainsKey(y)) dy[y].Add(name);                                // θ(1)
-                        else dy.Add(y, new List<string>() {name});                             // θ(1)
-                        // Add Node name and corresponding dZ in dz Hash Table
-                        if (dz.ContainsKey(z)) dz[z].Add(name);                                // θ(1)
-                        else dz.Add(z, new List<string>() {name});                             // θ(1)
-
-                    }
-                }
+                success = UpdatePosition(name, comparer, bhNode);                              // θ(1)
+                success = UpdateUniqueName(bhNode);                                            // θ(1)
             }
 
+            return success;                                                                    // θ(1)
+        }
 
 
+        
+        private bool UpdatePosition(string name, NodeDistanceComparer comparer, Node bhNode)
+        {
+            Dictionary<double, List<string>> dx = new Dictionary<double, List<string>>();  // θ(1)
+            Dictionary<double, List<string>> dy = new Dictionary<double, List<string>>();  // θ(1)
+            Dictionary<double, List<string>> dz = new Dictionary<double, List<string>>();  // θ(1)
+
+            // Update position
+            double x = 0;                                                                  // θ(1)
+            double y = 0;                                                                  // θ(1)
+            double z = 0;                                                                  // θ(1)
+
+            if (m_model.PointObj.GetCoordCartesian(name, ref x, ref y, ref z) == 0)        // θ(1)
+            {
+                oM.Geometry.Point p = new oM.Geometry.Point() { X = x, Y = y, Z = z };     // θ(1)
+
+                if (!comparer.Equals(bhNode, (Node)p))                                     // θ(1)
+                {
+                    // Get BHoM vs ETABS differences in nodes coordinates
+                    x = bhNode.Position.X - x;                                             // θ(1)
+                    y = bhNode.Position.Y - y;                                             // θ(1)
+                    z = bhNode.Position.Z - z;                                             // θ(1)
+
+                    // Add Node name and corresponding dX in dx Hash Table
+                    if (dx.ContainsKey(x)) dx[x].Add(name);                                // θ(1)
+                    else dx.Add(x, new List<string>() { name });                           // θ(1)
+                    // Add Node name and corresponding dY in dy Hash Table
+                    if (dy.ContainsKey(y)) dy[y].Add(name);                                // θ(1)
+                    else dy.Add(y, new List<string>() { name });                           // θ(1)
+                    // Add Node name and corresponding dZ in dz Hash Table
+                    if (dz.ContainsKey(z)) dz[z].Add(name);                                // θ(1)
+                    else dz.Add(z, new List<string>() { name });                           // θ(1)
+
+                }
+            }
+     
             // 2. MOVE NODES GROUP-BY-GROUP  -  ** STREAMS **
 
             // dX Movement
             dx.ToList().ForEach(kvp =>                                                         // θ(n)
-            {
+                {
                 // 1. Select all nodes belonging to same group
                 kvp.Value.ForEach(pplbl => m_model.PointObj.SetSelected(pplbl.ToString(), true));
                 // 2. Move all selected nodes by same dX
                 m_model.EditGeneral.Move((double)kvp.Key, 0, 0);
                 // 3. Deselect all selected nodes
                 kvp.Value.ForEach(pplbl => m_model.PointObj.SetSelected(pplbl.ToString(), false));
-            });
+                });
 
-            // dY Movement
-            dy.ToList().ForEach(kvp =>                                                         // θ(n)
-            {
+                // dY Movement
+                dy.ToList().ForEach(kvp =>                                                         // θ(n)
+                {
                 // 1. Select all nodes belonging to same group
                 kvp.Value.ForEach(pplbl => m_model.PointObj.SetSelected(pplbl.ToString(), true));
                 // 2. Move all selected nodes by same dY
                 m_model.EditGeneral.Move(0, (double)kvp.Key, 0);
                 // 3. Deselect all selected nodes
                 kvp.Value.ForEach(pplbl => m_model.PointObj.SetSelected(pplbl.ToString(), false));
-            });
+                });
 
-            // dZ Movement
-            dz.ToList().ForEach(kvp =>                                                         // θ(n)
-            {
+                // dZ Movement
+                dz.ToList().ForEach(kvp =>                                                         // θ(n)
+                {
                 // 1. Select all nodes belonging to same group
                 kvp.Value.ForEach(pplbl => m_model.PointObj.SetSelected(pplbl.ToString(), true));
                 // 2. Move all selected nodes by same dZ
                 m_model.EditGeneral.Move(0, 0, (double)kvp.Key);
                 // 3. Deselect all selected nodes
                 kvp.Value.ForEach(pplbl => m_model.PointObj.SetSelected(pplbl.ToString(), false));
-            });
+                });
 
-            return success;
+                return true;
+            }
+
+        /***************************************************/
+
+        [Description("Concatenates the last 7 characters of the ETABS Element GUID and the Node Name to get the Unique Name to assign to the ETABS Element.")]
+        private bool UpdateUniqueName(Node bhNode)
+        {
+            int ret01, ret02;
+            string guid = null;
+            string tempNodeName = "";
+
+            string uniqueName = GetAdapterId<string>(bhNode);
+
+            ret01 = m_model.PointObj.GetGUID(uniqueName, ref guid);
+
+            if (bhNode.Name == "")
+            {
+                tempNodeName = guid.Substring(guid.Length - 7);
+            }
+            else
+            {
+                tempNodeName = guid.Substring(guid.Length - 7) + "::" + bhNode.Name;
+            }
+
+            ret02 = m_model.PointObj.ChangeName(uniqueName, tempNodeName);
+
+            if (!(ret01 == 0 && ret02 == 0)) return false;
+
+            ETABSId etabsIdFragment = new ETABSId { Id = tempNodeName };
+
+            bhNode.SetAdapterId(etabsIdFragment);
+
+            return true;
         }
 
         /***************************************************/
