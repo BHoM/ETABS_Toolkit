788bb797258812363eb0ae51ea5724a279b54f58
Giorgio <giorgio.albieri@burohappold.com>
2025-11-24 17:44:44 +0000

    Update ETABS_oM/Elements/Tower.cs
    Change namespace from Elements to Fragments.
    
    Co-authored-by: Peter Nugent <Peter.Nugent@burohappold.com>

--- ETABS_oM/Elements/Tower.cs
+++ ETABS_oM/Elements/Tower.cs
@@ -29,7 +29,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace BH.oM.Adapters.ETABS.Elements
+namespace BH.oM.Adapters.ETABS.Fragments
 {
     public class Tower : BHoMObject, IFragment
     {
