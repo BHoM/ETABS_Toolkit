94a4b59c57936645a17ce171a70f72bf56f31fd3
Peter Nugent <Peter.Nugent@burohappold.com>
2026-03-30 10:09:50 +0100

    Update DataBaseSettings.cs

--- ETABS_oM/Settings/DataBaseSettings.cs
+++ ETABS_oM/Settings/DataBaseSettings.cs
@@ -31,7 +31,7 @@ using System.ComponentModel;
 
 namespace BH.oM.Adapters.ETABS
 {
-    public class DataBaseSettings : IObject
+    public class DatabaseSettings : IObject
     {
         /***************************************************/
         /**** Public Properties                         ****/
